d9256f0cd18fa4eef847aa81f5015c921bfabb59
Ben Langmead <ben.langmead@gmail.com>
2012-11-27 16:23:10 -0500

    Showing my work so far in using simple linear models to estimate mapping qualities for alignments.

---
+++ experiments/models/linear.R
@@ -0,0 +1,131 @@
+library(bitops)
+library(verification)
+
+# Open a tabulated sam file
+openTabulatedSam <- function(fn) {
+	return(read.table(fn, comment.char="", quote="", header=T))
+}
+
+# Given data frame of alignments, return data frame with just aligned reads
+selectAligned <- function(x) {
+	return(bitAnd(x$flag, 4) == 0)
+}
+
+# Given data frame of alignments, return just those that aligned once
+# (i.e. AS.i is defined but XS.i is not)
+selectAlignedOnce <- function(x) {
+	return(bitAnd(x$flag, 4) == 0 & is.na(x$XS.i))
+}
+
+# Given data frame of alignments, return just those that aligned twice
+# or more (i.e. AS.i and XS.i are defined)
+selectAlignedMoreThanOnce <- function(x) {
+	return(bitAnd(x$flag, 4) == 0 & !is.na(x$XS.i))
+}
+
+# Given data frame containing alignments that aligned just once, return
+# a fit for a linear model with response = mapq ranking and terms =
+# alignment score.
+modelAlignedOnce <- function(x) {
+	model <- lm(ZC.i ~ AS.i, x)
+	mapq <- model$coefficients[1] + model$coefficients[2] * x$AS.i
+	return(list(model=model, mapq=mapq))
+}
+
+# Given data frame containing alignments that aligned just once, return
+# a fit for a linear model with response = mapq ranking and terms =
+# alignment score.
+modelAlignedMoreThanOnce <- function(x) {
+	x$AS.iMXS.i <- x$AS.i - x$XS.i
+	model <- lm(ZC.i ~ AS.i + AS.iMXS.i, x)
+	mapq <- model$coefficients[1] + model$coefficients[2] * x$AS.i + model$coefficients[3] * x$AS.iMXS.i
+	return(list(model=model, mapq=mapq))
+}
+
+# Given a data frame containing all aligned reads (including those that
+# align just once or many times) return a fit for a linear model with
+# response = mapq ranking and terms = alignment scores.
+#
+# Second argument is a scaling factor, which determines how XS.i values
+# should be assigned to reads that align only once.  Right now we set
+# them equal to max(AS.i) - 2 * (max(AS.i) - min(AS.i)).  
+modelAltogether <- function(x, sca=2.0) {
+	repl <- max(x$AS.i) - sca * (max(x$AS.i) - min(x$AS.i))
+	xs <- ifelse(is.na(x$XS.i), repl, x$XS.i)
+	x$AS.iMXS.i <- x$AS.i - xs
+	model <- lm(ZC.i ~ AS.i + AS.iMXS.i, x)
+	mapq <- model$coefficients[1] + model$coefficients[2] * x$AS.i + model$coefficients[3] * x$AS.iMXS.i
+	return(list(model=model, mapq=mapq))
+}
+
+# Standardize the given vector so that all the elements are on
+# [0.0, 1.0]
+bt0and1 <- function(x) {
+	x <- x - min(x[!is.na(x)])
+	x <- x / max(x[!is.na(x)])
+	return(x)
+}
+
+# Plot the ZC.i values sorted by mapq, along with mapqs
+plotLinesAndDots <- function(x, plotReps=F) {
+	ordr <- order(x$mapq)
+	plot(jitter(x$ZC.i[ordr]), col=rgb(0, 0, 0, 0.1))
+	mapq <- bt0and1(x$mapq[ordr])
+	points(mapq, col=rgb(1.0, 0.0, 0.0, 0.1))
+	xsi <- bt0and1(x$XS.i[ordr])
+	asi <- bt0and1(x$AS.i[ordr])
+	points(asi - xsi, col=rgb(0.0, 0.0, 1.0, 0.1))
+	points(asi, col=rgb(1.0, 0.5, 0.25, 0.1))
+	if(plotReps) {
+		re <- bt0and1(x$AllRepeats[ordr])
+		points(re, col=rgb(0.0, 0.5, 0.0, 0.1))
+	}
+}
+
+# Assess quality of ranking.  In this case we add up all absolute
+# differences between element at rank i, and N/i where N is the total
+# number of elements.
+rankingError1 <- function(x) {
+	ordr <- order(x$mapq)
+	dif <- x$ZC.i[ordr] - seq(0, 1, length.out=nrow(x))
+	return(sum(abs(dif)))
+}
+
+# Another way to assess the quality of ranking.  Here for each
+# incorrect alignment we add its rank to the penalty, where the
+# worst-ranked alignment has rank 1.
+rankingError2 <- function(x) {
+	ordr <- order(x$mapq)
+	return(sum(which(x$ZC.i[ordr] == 0)))
+}
+
+# Plot a histogram of the mapqs for the incorrectly aligned reads. 
+incorrectMapqHist <- function(x) { hist(x$mapq[x$ZC.i == 0]) }
+
+# Return a table of the mapqs for the incorrectly aligned reads 
+incorrectMapqTable <- function(x) { return(table(x$mapq[x$ZC.i == 0])) }
+
+# Plot a ROC.  Not really appropriate for comparing two different sets
+# of alignments, since the "hit rate" and "false alarm rate" ignore the
+# fact that one set of alignments might contain more/different
+# alignments than the other.  A similar function that simply plots the
+# number of correct alignments on the vertical axis and number of
+# incorrect alignments on the horizontal axis would be better. 
+plotRoc <- function(x) {
+	roc.plot(x$ZC.i, x$mapq)
+}
+
+# Given filenames for SAM files emitted by two tools, analyze 
+fitMapqModels <- function(x) {
+	tab <- openTabulatedSam(x)
+	tab.1 <- tab[selectAlignedOnce(tab),]
+	tab.2 <- tab[selectAlignedMoreThanOnce(tab),]
+	tab.all <- rbind(tab.1, tab.2)
+	fit.1 <- modelAlignedOnce(tab.1); tab.1$mapq <- bt0and1(fit.1$mapq)
+	fit.2 <- modelAlignedMoreThanOnce(tab.2); tab.2$mapq <- bt0and1(fit.2$mapq)
+	fit.all <- modelAltogether(tab.all, sca=2.0); tab.all$mapq <- bt0and1(fit.all$mapq)
+	rankingError(tab.all)
+	rankingError(rbind(tab.1, tab.2))
+}
+
+x <- "/tmp/r0_ill_100_100k.bt2_s.sat"
