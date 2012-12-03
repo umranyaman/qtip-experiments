b548e3eee8c5eadc66f10d493e7b8b2a7a2a0ad1
Ben Langmead <ben.langmead@gmail.com>
2012-12-03 10:31:14 -0500

    Fixed problem with copy command

--- examples/mason/pull_unpaired.sh
+++ examples/mason/pull_unpaired.sh
@@ -1,1 +1,1 @@
-scp /scratch0/igm2/langmead/bowtie2/paper/experiments/mason/r0_ill_100_100k.bt2_s.sat.bz2 .
+scp igm1:/scratch0/igm2/langmead/bowtie2/paper/experiments/mason/r0_ill_100_100k.bt2_s.sat.bz2 .
