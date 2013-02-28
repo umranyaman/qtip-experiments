935e998adf8c049bcea12aad4075c53986f1f9a5
BenLangmead <ben.langmead@gmail.com>
2013-02-28 15:35:28 -0500

    Slight modification

--- examples/test_ts_human.sh
+++ examples/test_ts_human.sh
@@ -2,7 +2,7 @@
 
 BT2_HOME=$HOME/git/bowtie2/bowtie2
 
-REF=$HOME/fasta/hg19/chr1.fa
+REF=$HOME/bowtie2_indexes/Homo_sapiens.GRCh37.60.dna.fa
 IDX=$HOME/bowtie2_indexes/Homo_sapiens.GRCh37.60.dna.fa
 UNP_IN=$HOME/reads/SRA_HISEQ2000_FC1.shuffle.2M.1.fastq
 SAM_OUT=test_ts_human.sam
