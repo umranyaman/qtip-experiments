8064c354540835103a3c96840f868a0e97eea180
BenLangmead <ben.langmead@gmail.com>
2013-12-05 13:59:49 -0500

    cleaning up

--- examples/mason/pull_unpaired.sh
+++
@@ -1,2 +0,0 @@
-[ ! -f r0_ill_100_100k.bt2_s.sat.bz2 ] && scp igm1:/scratch0/igm2/langmead/bowtie2/paper/experiments/mason/r0_ill_100_100k.bt2_s.sat.bz2 .
-[ ! -f r0_ill_100_100k.bwa.sat.bz2 ] && scp igm1:/scratch0/igm2/langmead/bowtie2/paper/experiments/mason/r0_ill_100_100k.bwa.sat.bz2 .
