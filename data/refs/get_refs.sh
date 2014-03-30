d97e3d0489e92b592bda591befa3d424eb13307e
BenLangmead <ben.langmead@gmail.com>
2014-03-29 22:44:54 -0400

    refinement of ref downloading script

--- data/refs/get_refs.sh
+++ data/refs/get_refs.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
-GRCh37=ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz
-GRCm38=ftp://ftp.ensembl.org/pub/release-75/fasta/mus_musculus/dna/Mus_musculus.GRCm38.75.dna.primary_assembly.fa.gz
-ZM=ftp://ftp.ensemblgenomes.org/pub/plants/release-21/fasta/zea_mays/dna/Zea_mays.AGPv3.21.dna.genome.fa.gz
+GRCh37_FA=Homo_sapiens.GRCh37.75.dna.primary_assembly.fa
+GRCh37_URL=ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/$GRCh37_FA.gz
 
-wget $GRCh37
+GRCm38_FA=Mus_musculus.GRCm38.75.dna.primary_assembly.fa
+GRCm38_URL=ftp://ftp.ensembl.org/pub/release-75/fasta/mus_musculus/dna/$GRCm38_FA.gz
 
-wget $GRCm38
+ZM_FA=Zea_mays.AGPv3.21.dna.genome.fa
+ZM_URL=ftp://ftp.ensemblgenomes.org/pub/plants/release-21/fasta/zea_mays/dna/$ZM_FA.gz
 
-wget $ZM
+if [ ! -f $GRCh37_FA ] ; then
+    rm -f $GRCh37_FA.gz
+    wget $GRCh37_URL
+    gunzip $GRCh37_FA.gz
+fi
+
+if [ ! -f $GRCm38_FA ] ; then
+    rm -f $GRCm38_FA.gz
+    wget $GRCm38_URL
+    gunzip $GRCm38_FA.gz
+fi
+
+if [ ! -f $ZM_FA ] ; then
+    rm -f $ZM_FA.gz
+    wget $ZM_URL
+    gunzip $ZM_FA.gz
+fi
