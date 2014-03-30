0ab632fbe9e325cf40c47f1f3caf25339d82863e
BenLangmead <ben.langmead@gmail.com>
2014-03-30 11:16:14 -0400

    changed memory ceiling

--- data/refs/submit_index.sh
+++ data/refs/submit_index.sh
@@ -4,9 +4,10 @@ cat > .hg19.sh <<EOF
 #PBS -q batch
 #PBS -l walltime=10:00:00
 #PBS -j n
-#PBS -l pmem=12gb
-#PBS -l vmem=12gb
-#PBS -l mem=12gb
+#PBS -l pmem=8gb
+#PBS -l vmem=8gb
+#PBS -l pvmem=8gb
+#PBS -l mem=8gb
 cd $PWD
 ulimit -v 8388608
 bowtie2-build --bmax 537647674 --dcv 1024 $TS_REFS/hg19.fa hg19.fa
@@ -17,9 +18,10 @@ cat > .mm10.sh <<EOF
 #PBS -q batch
 #PBS -l walltime=10:00:00
 #PBS -j n
-#PBS -l pmem=12gb
-#PBS -l vmem=12gb
-#PBS -l mem=12gb
+#PBS -l pmem=8gb
+#PBS -l vmem=8gb
+#PBS -l pvmem=8gb
+#PBS -l mem=8gb
 cd $PWD
 ulimit -v 8388608
 bowtie2-build --bmax 537647674 --dcv 1024 $TS_REFS/mm10.fa mm10.fa
@@ -30,9 +32,10 @@ cat > .zm_AGPv3.sh <<EOF
 #PBS -q batch
 #PBS -l walltime=10:00:00
 #PBS -j n
-#PBS -l pmem=12gb
-#PBS -l vmem=12gb
-#PBS -l mem=12gb
+#PBS -l pmem=8gb
+#PBS -l vmem=8gb
+#PBS -l pvmem=8gb
+#PBS -l mem=8gb
 cd $PWD
 ulimit -v 8388608
 bowtie2-build --bmax 537647674 --dcv 1024 $TS_REFS/zm_AGPv3.fa zm_AGPv3.fa
