8fff9e110ef37f002acb1f319101dc73634fa51a
BenLangmead <ben.langmead@gmail.com>
2014-03-29 23:40:26 -0400

    script for indexing genomes

---
+++ data/refs/submit_index.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cat > .hg19.sh <<EOF
+#PBS -q batch
+#PBS -l walltime=10:00:00
+#PBS -j n
+#PBS -l pmem=8gb
+bowtie2-build $TS_REFS/hg19.fa hg19.fa
+EOF
+echo qsub .hg19.sh
+
+cat > .mm10.sh <<EOF
+#PBS -q batch
+#PBS -l walltime=10:00:00
+#PBS -j n
+#PBS -l pmem=8gb
+bowtie2-build $TS_REFS/mm10.fa mm10.fa
+EOF
+echo qsub .mm10.sh
+
+cat > .zm_AGPv3.sh <<EOF
+#PBS -q batch
+#PBS -l walltime=10:00:00
+#PBS -j n
+#PBS -l pmem=8gb
+bowtie2-build $TS_REFS/zm_AGPv3.fa zm_AGPv3.fa
+EOF
+echo qsub .zm_AGPv3.sh
