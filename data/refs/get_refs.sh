95d7ebd705d213a8290c8eec62581997a15260c8
BenLangmead <ben.langmead@gmail.com>
2014-03-30 22:51:45 -0400

    added fa indexing

--- data/refs/get_refs.sh
+++ data/refs/get_refs.sh
@@ -13,16 +13,22 @@ if [ ! -f $GRCh37_FA ] ; then
     rm -f $GRCh37_FA.gz
     wget $GRCh37_URL
     gunzip $GRCh37_FA.gz
+    samtools faidx $GRCh37_FA
 fi
+ln -s -f $GRCh37_FA hg19.fa
 
 if [ ! -f $GRCm38_FA ] ; then
     rm -f $GRCm38_FA.gz
     wget $GRCm38_URL
     gunzip $GRCm38_FA.gz
+    samtools faidx $GRCm38_FA
 fi
+ln -s -f $GRCm38_FA mm10.fa
 
 if [ ! -f $ZM_FA ] ; then
     rm -f $ZM_FA.gz
     wget $ZM_URL
     gunzip $ZM_FA.gz
+    samtools faidx $ZM_FA
 fi
+ln -s -f $ZM_FA zm_AGPv3.fa
