1cdbd7f014513faeb9ffb56b342aaab87f7e642f
BenLangmead <ben.langmead@gmail.com>
2014-03-30 23:51:33 -0400

    added BWA indexing

--- data/refs/submit_index.sh
+++ data/refs/submit_index.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-cat > .hg19.sh <<EOF
+[ -z "$TS_HOME" ] && echo "Set TS_HOME" && exit 1
+
+cat > .hg19.bt2.sh <<EOF
 #PBS -q batch
 #PBS -l walltime=10:00:00
 #PBS -j n
@@ -12,9 +14,22 @@ cd $PWD
 ulimit -v 8388608
 bowtie2-build --bmax 537647674 --dcv 1024 $TS_REFS/hg19.fa hg19.fa
 EOF
-echo qsub .hg19.sh
+echo qsub .hg19.bt2.sh
 
-cat > .mm10.sh <<EOF
+cat > .hg19.bwa.sh <<EOF
+#PBS -q batch
+#PBS -l walltime=10:00:00
+#PBS -j n
+#PBS -l pmem=8gb
+#PBS -l vmem=8gb
+#PBS -l pvmem=8gb
+#PBS -l mem=8gb
+cd $PWD
+bwa index $TS_REFS/hg19.fa
+EOF
+echo qsub .hg19.bwa.sh
+
+cat > .mm10.bt2.sh <<EOF
 #PBS -q batch
 #PBS -l walltime=10:00:00
 #PBS -j n
@@ -26,9 +41,22 @@ cd $PWD
 ulimit -v 8388608
 bowtie2-build --bmax 537647674 --dcv 1024 $TS_REFS/mm10.fa mm10.fa
 EOF
-echo qsub .mm10.sh
+echo qsub .mm10.bt2.sh
 
-cat > .zm_AGPv3.sh <<EOF
+cat > .mm10.bwa.sh <<EOF
+#PBS -q batch
+#PBS -l walltime=10:00:00
+#PBS -j n
+#PBS -l pmem=8gb
+#PBS -l vmem=8gb
+#PBS -l pvmem=8gb
+#PBS -l mem=8gb
+cd $PWD
+bwa index $TS_REFS/mm10.fa
+EOF
+echo qsub .mm10.bwa.sh
+
+cat > .zm_AGPv3.bt2.sh <<EOF
 #PBS -q batch
 #PBS -l walltime=10:00:00
 #PBS -j n
@@ -40,4 +68,17 @@ cd $PWD
 ulimit -v 8388608
 bowtie2-build --bmax 537647674 --dcv 1024 $TS_REFS/zm_AGPv3.fa zm_AGPv3.fa
 EOF
-echo qsub .zm_AGPv3.sh
+echo qsub .zm_AGPv3.bt2.sh
+
+cat > .zm_AGPv3.bwa.sh <<EOF
+#PBS -q batch
+#PBS -l walltime=10:00:00
+#PBS -j n
+#PBS -l pmem=8gb
+#PBS -l vmem=8gb
+#PBS -l pvmem=8gb
+#PBS -l mem=8gb
+cd $PWD
+bwa index $TS_REFS/zm_AGPv3.fa
+EOF
+echo qsub .zm_AGPv3.bwa.sh
