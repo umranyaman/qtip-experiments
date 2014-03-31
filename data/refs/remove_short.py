3a313c9112ee4e4ea347b5b6e69170650420725d
BenLangmead <ben.langmead@gmail.com>
2014-03-30 23:36:33 -0400

    beginnings of a script to remove short sequences from FASTA since Mason doesn't like them

---
+++ data/refs/remove_short.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+__author__ = 'langmead'
+
+import sys
+
+name = None
+length = 0
+max_len = 500
+first = True
+for ln in sys.stdin:
+    if ln[0] == '>':
+        if not first and length < max_len:
+            print 'Ref %s has length %d' % (name, length)
+        first = False
+        length = 0
+        name = ln[1:].strip()[0]
+    else:
+        length += len(ln.rstrip())
+if not first and length < max_len:
+    print 'Ref %s has length %d' % (name, length)
