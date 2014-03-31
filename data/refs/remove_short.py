94399b10c0ee40ff9356c26c70776dfd81c4a4c0
BenLangmead <ben.langmead@gmail.com>
2014-03-30 23:44:40 -0400

    initial version

--- data/refs/remove_short.py
+++ data/refs/remove_short.py
@@ -6,16 +6,30 @@ import sys
 
 name = None
 length = 0
-max_len = 500
+max_len = 10000
 first = True
+buf = []
 for ln in sys.stdin:
     if ln[0] == '>':
-        if not first and length < max_len:
-            print 'Ref %s has length %d' % (name, length)
+        if not first:
+            if length < max_len:
+                print >> sys.stderr, 'Ref %s has length %d' % (name, length)
+            else:
+                print '>%s' % name
+                print '\n'.join(buf)
+                buf = []
         first = False
         length = 0
-        name = ln[1:].split()[0]
+        name = ln[1:]
     else:
-        length += len(ln.rstrip())
-if not first and length < max_len:
-    print 'Ref %s has length %d' % (name, length)
+        ln = ln.rstrip()
+        length += len(ln)
+        buf.append(ln)
+
+if not first:
+    if length < max_len:
+        print >> sys.stderr, 'Ref %s has length %d' % (name, length)
+    else:
+        print '>%s' % name
+        print '\n'.join(buf)
+        buf = []
