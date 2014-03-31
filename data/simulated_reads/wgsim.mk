20b5357b40b4fa8f0101c6449bcec6025d3d2ac8
BenLangmead <ben.langmead@gmail.com>
2014-03-30 23:13:47 -0400

    fixed variable naming problem that would cause concurrent targets to stomp on each other's intermediate files

--- data/simulated_reads/wgsim.mk
+++ data/simulated_reads/wgsim.mk
@@ -24,8 +24,8 @@ define wgsim_ill_unp_reads
 
 # Generate unpaired reads
 r0_wgsim_$1.fq.gz: $$(FA) $$(TS_HOME)/software/wgsim/wgsim
-	$$(TS_HOME)/software/wgsim/wgsim -S $5 -1 $3 -2 $3 -N $4 $2 $$(@:%.fq.gz=%.fq) .tmp
-	rm -f .tmp
+	$$(TS_HOME)/software/wgsim/wgsim -S $5 -1 $3 -2 $3 -N $4 $2 $$(@:%.fq.gz=%.fq) .tmp.$$@
+	rm -f .tmp.$$@
 	gzip $$(@:%.fq.gz=%.fq)
 
 endef
@@ -38,11 +38,11 @@ define wgsim_ill_pair_reads
 
 # Generate paired-end reads
 r1_wgsim_$1.fq.gz: $(FA) $$(TS_HOME)/software/wgsim/wgsim
-	$$(TS_HOME)/software/wgsim/wgsim -S $7 -d $5 -s $6 -1 $3 -2 $3 -N $4 $2 .tmp_1.fq .tmp_2.fq
-	gzip -c .tmp_1.fq > $$@
-	rm -f .tmp_1.fq
-	gzip -c .tmp_2.fq > $$(@:r1_%=r2_%)
-	rm -f .tmp_2.fq
+	$$(TS_HOME)/software/wgsim/wgsim -S $7 -d $5 -s $6 -1 $3 -2 $3 -N $4 $2 .tmp_1.$$@.fq .tmp_2.$$@.fq
+	gzip -c .tmp_1.$$@.fq > $$@
+	rm -f .tmp_1.$$@.fq
+	gzip -c .tmp_2.$$@.fq > $$(@:r1_%=r2_%)
+	rm -f .tmp_2.$$@.fq
 
 endef
 
