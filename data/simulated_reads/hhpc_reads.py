aebf494217535ecc35e0631f473400f565381794
BenLangmead <ben.langmead@gmail.com>
2014-03-31 21:26:38 -0400

    changes in preparation for batch submission of tss jobs

--- data/simulated_reads/hhpc_reads.py
+++ data/simulated_reads/hhpc_reads.py
@@ -3,9 +3,11 @@
 import os
 import sys
 import time
+import re
 
 
 idx = 0
+tss_re = re.compile('tss[_a-zA-Z01-9]*:.*')
 
 
 def handle_dir(dirname, dry_run=True):
@@ -13,34 +15,35 @@ def handle_dir(dirname, dry_run=True):
     with open(os.path.join(dirname, 'Makefile')) as fh:
         in_reads = False
         for ln in fh:
-            if ln.startswith('reads:'):
+            if tss_re.match(ln):
                 in_reads = True
             elif in_reads:
                 if len(ln.rstrip()) == 0:
-                    break
-                target = ln.split()[0]
-                print >> sys.stderr, '  Found a read file: %s' % target
-                pbs_lns = list()
-                pbs_lns.append('#PBS -q batch')
-                pbs_lns.append('#PBS -l walltime=30:00')
-                pbs_lns.append('#PBS -j n')
-                pbs_lns.append('#PBS -l pmem=32gb')
-                pbs_lns.append('#PBS -l vmem=32gb')
-                pbs_lns.append('#PBS -l pvmem=32gb')
-                pbs_lns.append('#PBS -l mem=32gb')
-                pbs_lns.append('export TS_HOME=%s' % os.environ['TS_HOME'])
-                pbs_lns.append('export TS_INDEXES=%s' % os.environ['TS_INDEXES'])
-                pbs_lns.append('export TS_REFS=%s' % os.environ['TS_REFS'])
-                pbs_lns.append('cd %s' % os.path.abspath(dirname))
-                pbs_lns.append('make %s' % target)
-                qsub_fn = '.%s.%d.sh' % (target, idx)
-                with open(qsub_fn, 'w') as ofh:
-                    ofh.write('\n'.join(pbs_lns) + '\n')
-                idx += 1
-                print 'qsub %s' % qsub_fn
-                if not dry_run:
-                    os.system('qsub %s' % qsub_fn)
-                    time.sleep(0.2)
+                    in_reads = False
+                else:
+                    target = ln.split()[0]
+                    print >> sys.stderr, '  Found a read file: %s' % target
+                    pbs_lns = list()
+                    pbs_lns.append('#PBS -q batch')
+                    pbs_lns.append('#PBS -l walltime=1:30:00')
+                    pbs_lns.append('#PBS -j n')
+                    pbs_lns.append('#PBS -l pmem=8gb')
+                    pbs_lns.append('#PBS -l vmem=8gb')
+                    pbs_lns.append('#PBS -l pvmem=8gb')
+                    pbs_lns.append('#PBS -l mem=8gb')
+                    pbs_lns.append('export TS_HOME=%s' % os.environ['TS_HOME'])
+                    pbs_lns.append('export TS_INDEXES=%s' % os.environ['TS_INDEXES'])
+                    pbs_lns.append('export TS_REFS=%s' % os.environ['TS_REFS'])
+                    pbs_lns.append('cd %s' % os.path.abspath(dirname))
+                    pbs_lns.append('make %s' % target)
+                    qsub_fn = '.%s.%d.sh' % (target, idx)
+                    with open(qsub_fn, 'w') as ofh:
+                        ofh.write('\n'.join(pbs_lns) + '\n')
+                    idx += 1
+                    print 'qsub %s' % qsub_fn
+                    if not dry_run:
+                        os.system('qsub %s' % qsub_fn)
+                        time.sleep(0.2)
 
 
 def go():
@@ -53,6 +56,6 @@ def go():
     for dirname, dirs, files in os.walk('.'):
         if 'Makefile' in files:
             print >> sys.stderr, 'Found a Makefile: %s' % (os.path.join(dirname, 'Makefile'))
-            handle_dir(dirname, dry_run=len(sys.argv) > 1)
+            handle_dir(dirname, dry_run=(len(sys.argv) > 1))
 
 go()
--- data/simulated_reads/hhpc_tss.py
+++ data/simulated_reads/hhpc_tss.py
@@ -2,24 +2,58 @@
 
 import os
 import sys
+import time
 
 
-def handle_dir(dirname):
+idx = 0
+
+
+def handle_dir(dirname, dry_run=True):
+    global idx
     with open(os.path.join(dirname, 'Makefile')) as fh:
-        in_tss = False
+        in_reads = False
         for ln in fh:
-            if ln.startswith('tss:'):
-                in_tss = True
-            elif in_tss:
+            if ln.startswith('reads:'):
+                in_reads = True
+            elif in_reads:
                 if len(ln.rstrip()) == 0:
-                    break
-                print >> sys.stderr, '  Found a ts target: %s' % ln.split()[0]
+                    in_reads = False
+                else:
+                    target = ln.split()[0]
+                    print >> sys.stderr, '  Found a read file: %s' % target
+                    pbs_lns = list()
+                    pbs_lns.append('#PBS -q batch')
+                    pbs_lns.append('#PBS -l walltime=30:00')
+                    pbs_lns.append('#PBS -j n')
+                    pbs_lns.append('#PBS -l pmem=32gb')
+                    pbs_lns.append('#PBS -l vmem=32gb')
+                    pbs_lns.append('#PBS -l pvmem=32gb')
+                    pbs_lns.append('#PBS -l mem=32gb')
+                    pbs_lns.append('export TS_HOME=%s' % os.environ['TS_HOME'])
+                    pbs_lns.append('export TS_INDEXES=%s' % os.environ['TS_INDEXES'])
+                    pbs_lns.append('export TS_REFS=%s' % os.environ['TS_REFS'])
+                    pbs_lns.append('cd %s' % os.path.abspath(dirname))
+                    pbs_lns.append('make %s' % target)
+                    qsub_fn = '.%s.%d.sh' % (target, idx)
+                    with open(qsub_fn, 'w') as ofh:
+                        ofh.write('\n'.join(pbs_lns) + '\n')
+                    idx += 1
+                    print 'qsub %s' % qsub_fn
+                    if not dry_run:
+                        os.system('qsub %s' % qsub_fn)
+                        time.sleep(0.2)
 
 
 def go():
+    if 'TS_HOME' not in os.environ:
+        raise RuntimeError('Must have TS_HOME set')
+    if 'TS_REFS' not in os.environ:
+        raise RuntimeError('Must have TS_REFS set')
+    if 'TS_INDEXES' not in os.environ:
+        raise RuntimeError('Must have TS_INDEXES set')
     for dirname, dirs, files in os.walk('.'):
         if 'Makefile' in files:
             print >> sys.stderr, 'Found a Makefile: %s' % (os.path.join(dirname, 'Makefile'))
-            handle_dir(dirname)
+            handle_dir(dirname, dry_run=len(sys.argv) > 1)
 
 go()
