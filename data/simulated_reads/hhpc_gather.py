228a1e9c3eb69c717dc5a8cbdd9a270278d348d7
BenLangmead <ben.langmead@gmail.com>
2014-04-09 16:07:56 -0400

    fixed filename issue

--- data/simulated_reads/hhpc_gather.py
+++ data/simulated_reads/hhpc_gather.py
@@ -35,11 +35,12 @@ def handle_dir(dirname):
                     target_full = os.path.join(dirname, target)
 
                     # subsampling png
-                    subsampling_png_fn = '%s_%s_subsampling_series.png' % (name, target)
-                    subsampling_png_full = os.path.join(target_full, subsampling_png_fn)
-                    subsampling_png_out = os.path.join('summary/subsampling_plots')
+                    subsampling_png_src_fn = 'subsampling_series.png' % (name, target)
+                    subsampling_png_dst_fn = '%s_%s_subsampling_series.png' % (name, target)
+                    subsampling_png_full = os.path.join(target_full, subsampling_png_src_fn)
+                    subsampling_png_out = os.path.join('summary', 'subsampling_plots', subsampling_png_dst_fn)
                     mkdir_quiet(subsampling_png_out)
-                    shutil.copy(subsampling_png_full, subsampling_png_out)
+                    shutil.copyfile(subsampling_png_full, subsampling_png_out)
 
 
 def go():
