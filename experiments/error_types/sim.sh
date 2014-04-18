b89c40ab0a458d200b8a568f41c6ceaeb6e8861c
BenLangmead <ben.langmead@gmail.com>
2014-04-17 22:48:14 -0400

    don't needlessly regenerate

--- experiments/error_types/sim.sh
+++ experiments/error_types/sim.sh
@@ -25,51 +25,56 @@ CONTAM3_FA=$TS_REFS/mm10.fa
 NUM_READS=910000
 NUM_CONTAM_READS=30000
 
-$TS_HOME/software/mason/mason illumina \
-	--num-reads $NUM_READS \
-	--num-haplotypes 2 \
-	--include-read-information \
-	--seed 345 \
-	--simulate-qualities \
-	--read-length 100 \
-	--output-file hg19.sim.fq \
-	$HG19_FA
+if [ ! -f hg19.sim.fq ] ; then
+    $TS_HOME/software/mason/mason illumina \
+        --num-reads $NUM_READS \
+        --num-haplotypes 2 \
+        --include-read-information \
+        --seed 345 \
+        --simulate-qualities \
+        --read-length 100 \
+        --output-file hg19.sim.fq \
+        $HG19_FA
+fi
 
-$TS_HOME/software/mason/mason illumina \
-	--num-reads $NUM_CONTAM_READS \
-	--num-haplotypes 1 \
-	--include-read-information \
-	--seed 254 \
-	--simulate-qualities \
-	--read-length 100 \
-	--output-file .contam1.sim.fq \
-	$CONTAM1_FA
+if [ ! -f contam1.sim.fq ] ; then
+    $TS_HOME/software/mason/mason illumina \
+        --num-reads $NUM_CONTAM_READS \
+        --num-haplotypes 1 \
+        --include-read-information \
+        --seed 254 \
+        --simulate-qualities \
+        --read-length 100 \
+        --output-file .contam1.sim.fq \
+        $CONTAM1_FA
+    sed 's/contig=/contig=lambda-/' < .contam1.sim.fq > contam1.sim.fq
+    rm -f .contam1.sim.fq
+fi
 
-sed 's/contig=/contig=lambda-/' < .contam1.sim.fq > contam1.sim.fq
-rm -f .contam1.sim.fq
+if [ ! -f contam2.sim.fq ] ; then
+    $TS_HOME/software/mason/mason illumina \
+        --num-reads $NUM_CONTAM_READS \
+        --num-haplotypes 1 \
+        --include-read-information \
+        --seed 298 \
+        --simulate-qualities \
+        --read-length 100 \
+        --output-file .contam2.sim.fq \
+        $CONTAM2_FA
+    sed 's/contig=/contig=e_coli-/' < .contam2.sim.fq > contam2.sim.fq
+    rm -f .contam2.sim.fq
+fi
 
-$TS_HOME/software/mason/mason illumina \
-	--num-reads $NUM_CONTAM_READS \
-	--num-haplotypes 1 \
-	--include-read-information \
-	--seed 298 \
-	--simulate-qualities \
-	--read-length 100 \
-	--output-file .contam2.sim.fq \
-	$CONTAM2_FA
-
-sed 's/contig=/contig=e_coli-/' < .contam2.sim.fq > contam2.sim.fq
-rm -f .contam2.sim.fq
-
-$TS_HOME/software/mason/mason illumina \
-	--num-reads $NUM_CONTAM_READS \
-	--num-haplotypes 2 \
-	--include-read-information \
-	--seed 046 \
-	--simulate-qualities \
-	--read-length 100 \
-	--output-file .contam3.sim.fq \
-	$CONTAM3_FA
-
-sed 's/contig=/contig=mouse-/' < .contam3.sim.fq > contam3.sim.fq
-rm -f .contam3.sim.fq
+if [ ! -f contam3.sim.fq ] ; then
+    $TS_HOME/software/mason/mason illumina \
+        --num-reads $NUM_CONTAM_READS \
+        --num-haplotypes 2 \
+        --include-read-information \
+        --seed 046 \
+        --simulate-qualities \
+        --read-length 100 \
+        --output-file .contam3.sim.fq \
+        $CONTAM3_FA
+    sed 's/contig=/contig=mouse-/' < .contam3.sim.fq > contam3.sim.fq
+    rm -f .contam3.sim.fq
+fi
