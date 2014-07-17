b4a7191ee52eebd05f81ebf2985ca02dc5e9e403
BenLangmead <ben.langmead@gmail.com>
2014-07-16 20:35:28 -0400

    added --write-all

--- data/simulated_reads/predict.mk
+++ data/simulated_reads/predict.mk
@@ -1,5 +1,5 @@
 PRED=python $(TS_HOME)/bin/predict.py
-PRED_ARGS=
+PRED_ARGS=--write-all
 
 %.pred: %.ts
 	$(PRED) --in $< --out $@ $(PRED_ARGS) --verbose --subsampling-fraction=0.3 --plot-all --subsampling-series 0.05,0.1,0.2,0.3,0.4,0.5,1.0 --subsampling-repl=5
