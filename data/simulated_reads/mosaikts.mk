03411fff0125aa145bc695e01070e2c23237e10b
BenLangmead <ben.langmead@gmail.com>
2014-12-14 22:05:55 -0500

    add extension for index

--- data/simulated_reads/mosaikts.mk
+++ data/simulated_reads/mosaikts.mk
@@ -13,7 +13,7 @@ define mosaikts
 
 r0_$1_%.ts: r0_%.fq.mkb
 	$$(TS) --ref $6 --mosaik-align-exe $$(MOSAIK_ALIGN) --mosaik-build-exe $$(MOSAIK_BUILD) --aligner=mosaik \
-           --index $7 --sim-fraction 0.01 --sim-unp-min $2 $$(TS_ARGS) $4 --output-directory $$@ --U $$< \
+           --index $7.mosaik --sim-fraction 0.01 --sim-unp-min $2 $$(TS_ARGS) $4 --output-directory $$@ --U $$< \
            -- $5 $$(MOSAIK_ALIGN_ARGS) \
                  -annpe $(MOSAIK_HOME)/MOSAIK/networkFile/2.1.26.pe.100.0065.ann \
                  -annse $(MOSAIK_HOME)/MOSAIK/networkFile/2.1.26.se.100.005.ann
@@ -22,7 +22,7 @@ r0_$1_%.ts: r0_%.fq.mkb
 	$$(TS) --version > $$@/ts_version
 
 r12_$1_%.ts: r12_%.fq.mkb
-	$$(TS) --ref $6 --bwa-exe $$(BWA) --aligner=bwa-mem  --index $7 --sim-fraction 0.01 \
+	$$(TS) --ref $6 --bwa-exe $$(BWA) --aligner=bwa-mem  --index $7.mosaik --sim-fraction 0.01 \
 	       --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 $$(TS_ARGS) $4 \
 	       --output-directory $$@ --m1 $$< --m2 $$(<:r1_%=r2_%) \
            -- $5 $$(MOSAIK_ALIGN_ARGS) \
