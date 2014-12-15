e160af8bc6fb9a885309cb80baa4b11ac791e445
BenLangmead <ben.langmead@gmail.com>
2014-12-14 22:03:36 -0500

    adding annotation parameters for mosaik

--- data/simulated_reads/mosaikts.mk
+++ data/simulated_reads/mosaikts.mk
@@ -13,14 +13,21 @@ define mosaikts
 
 r0_$1_%.ts: r0_%.fq.mkb
 	$$(TS) --ref $6 --mosaik-align-exe $$(MOSAIK_ALIGN) --mosaik-build-exe $$(MOSAIK_BUILD) --aligner=mosaik \
-           --index $7 --sim-fraction 0.01 --sim-unp-min $2 $$(TS_ARGS) $4 --output-directory $$@ --U $$< -- \
-           $5 $$(MOSAIK_ARGS) $$(MOSAIK_EXTRA_ARGS)
+           --index $7 --sim-fraction 0.01 --sim-unp-min $2 $$(TS_ARGS) $4 --output-directory $$@ --U $$< \
+           -- $5 $$(MOSAIK_ALIGN_ARGS) \
+                 -annpe $(MOSAIK_HOME)/MOSAIK/networkFile/2.1.26.pe.100.0065.ann \
+                 -annse $(MOSAIK_HOME)/MOSAIK/networkFile/2.1.26.se.100.005.ann
 	-$$(MOSAIK_ALIGN) > $$@/mosaik_align_version 2>&1
 	-$$(MOSAIK_BUILD) > $$@/mosaik_build_version 2>&1
 	$$(TS) --version > $$@/ts_version
 
 r12_$1_%.ts: r12_%.fq.mkb
-	$$(TS) --ref $6 --bwa-exe $$(BWA) --aligner=bwa-mem  --index $7 --sim-fraction 0.01 --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 $$(TS_ARGS) $4 --output-directory $$@ --m1 $$< --m2 $$(<:r1_%=r2_%) -- $5 $$(BWA_ARGS) $$(BWA_EXTRA_ARGS)
+	$$(TS) --ref $6 --bwa-exe $$(BWA) --aligner=bwa-mem  --index $7 --sim-fraction 0.01 \
+	       --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 $$(TS_ARGS) $4 \
+	       --output-directory $$@ --m1 $$< --m2 $$(<:r1_%=r2_%) \
+           -- $5 $$(MOSAIK_ALIGN_ARGS) \
+                 -annpe $(MOSAIK_HOME)/MOSAIK/networkFile/2.1.26.pe.100.0065.ann \
+                 -annse $(MOSAIK_HOME)/MOSAIK/networkFile/2.1.26.se.100.005.ann
 	-$$(MOSAIK_ALIGN) > $$@/mosaik_align_version 2>&1
 	-$$(MOSAIK_BUILD) > $$@/mosaik_build_version 2>&1
 	$$(TS) --version > $$@/ts_version
