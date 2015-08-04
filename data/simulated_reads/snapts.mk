f5df7bab84c0ed8debdb3a1c23602fa659e6da39
BenLangmead <langmea@cs.jhu.edu>
2015-08-03 21:03:03 -0400

    fixing arguments to snap ts

--- data/simulated_reads/snapts.mk
+++ data/simulated_reads/snapts.mk
@@ -1,18 +1,28 @@
 # Must already have a SNAP index of the FASTA reference somewhere
 
+# Arguments:
+# 1. Experiment name
+# 2. Minimum # unpaired reads to simulate
+# 3. Minimum # discordant and bad-end reads to simulate
+# 4. Arguments to ts.py script
+# 5. Arguments for SNAP (in either single or paired mode)
+# 6. Arguments for SNAP in single mode
+# 7. Arguments for SNAP in paired mode
+# 8. Reference to use
+# 9. Index to use
+
 TS=python $(TS_HOME)/bin/ts.py
 TS_ARGS=--compress-output --verbose --write-all
-SNAP_EXTRA_ARGS=
 
 define snapts
 
 r0_$1_%.ts: r0_%.fq.gz
-	$$(TS) --ref $6 --snap-exe $$(SNAP) --aligner snap --index $7.snap --sim-fraction 0.01 --sim-unp-min $2 $$(TS_ARGS) $4 --output-directory $$@ --U $$< -- $5 $$(SNAP_ARGS) $$(SNAP_EXTRA_ARGS)
+	$$(TS) --ref $8 --snap-exe $$(SNAP) --aligner snap --index $9.snap --sim-fraction 0.01 --sim-unp-min $2 $$(TS_ARGS) $4 --output-directory $$@ --U $$< -- $5 $$(SNAP_ARGS) -- $6 -- $7
 	$$(SNAP) 2> $$@/snap_version
 	$$(TS) --version > $$@/ts_version
 
 r12_$1_%.ts: r1_%.fq.gz
-	$$(TS) --ref $6 --snap-exe $$(SNAP) --aligner snap --index $7.snap --sim-fraction 0.01 --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 $$(TS_ARGS) $4 --output-directory $$@ --m1 $$< --m2 $$(<:r1_%=r2_%) -- $5 $$(SNAP_ARGS) $$(SNAP_EXTRA_ARGS)
+	$$(TS) --ref $8 --snap-exe $$(SNAP) --aligner snap --index $9.snap --sim-fraction 0.01 --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 $$(TS_ARGS) $4 --output-directory $$@ --m1 $$< --m2 $$(<:r1_%=r2_%) -- $5 $$(SNAP_ARGS) -- $6 -- $7
 	$$(SNAP) 2> $$@/snap_version
 	$$(TS) --version > $$@/ts_version
 
