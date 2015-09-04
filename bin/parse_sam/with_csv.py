493b81a0ea8324eab828ee044f4131666fbf278e
BenLangmead <langmea@cs.jhu.edu>
2015-09-04 09:11:43 -0400

    updates to the parse_sam tests

--- bin/parse_sam/with_csv.py
+++ bin/parse_sam/with_csv.py
@@ -1,1 +1,22 @@
 __author__ = 'langmead'
+
+import csv
+import numpy
+
+mapqs = []
+zt_zs = []
+
+with open('input.sam', 'rb') as fh:
+    reader = csv.reader(fh, delimiter='\t', quotechar=None)
+    while True:
+        try:
+            row = reader.next()
+        except StopIteration:
+            break
+        if row[0][0] == '@':
+            continue
+        zt_zs.append(row[-1][5:])
+        mapqs.append(row[4])
+
+print(zt_zs[:10])
+print(mapqs[:10])
