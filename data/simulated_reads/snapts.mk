fe430e4cdc1a60eb0d15f4e384e1cc536b3d0d10
BenLangmead <langmea@cs.jhu.edu>
2015-09-28 15:09:22 -0400

    tweaks

--- data/simulated_reads/snapts.mk
+++ data/simulated_reads/snapts.mk
@@ -12,18 +12,17 @@
 # 9. Index to use
 
 TS=python $(TS_HOME)/src/ts.py
-TS_ARGS+=
 SNAP_ARGS+=-t 1
 
 define snapts
 
 r0_$1_%.ts: r0_%.fq.gz
-	$$(TS) --ref $8 --snap-exe $$(SNAP) --aligner snap --index $9.snap --sim-fraction 0.01 --sim-unp-min $2 $$(TS_ARGS) $4 --output-directory $$@ --U $$< -- $5 $$(SNAP_ARGS) -- $6 -- $7
+	$$(TS) --ref $8 --snap-exe $$(SNAP) --aligner snap --index $9.snap --sim-fraction 0.01 --sim-unp-min $2 $$(SNAP_TS_ARGS) $4 --output-directory $$@ --U $$< -- $5 $$(SNAP_ARGS) -- $6 -- $7
 	$$(SNAP) 2> $$@/snap_version
 	$$(TS) --version > $$@/ts_version
 
 r12_$1_%.ts: r1_%.fq.gz
-	$$(TS) --ref $8 --snap-exe $$(SNAP) --aligner snap --index $9.snap --sim-fraction 0.01 --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 $$(TS_ARGS) $4 --output-directory $$@ --m1 $$< --m2 $$(<:r1_%=r2_%) -- $5 $$(SNAP_ARGS) -- $6 -- $7
+	$$(TS) --ref $8 --snap-exe $$(SNAP) --aligner snap --index $9.snap --sim-fraction 0.01 --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 $$(SNAP_TS_ARGS) $4 --output-directory $$@ --m1 $$< --m2 $$(<:r1_%=r2_%) -- $5 $$(SNAP_ARGS) -- $6 -- $7
 	$$(SNAP) 2> $$@/snap_version
 	$$(TS) --version > $$@/ts_version
 
