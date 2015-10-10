89945793ba55dcd95e4c9517c23f406164f8c055
BenLangmead <langmea@cs.jhu.edu>
2015-10-10 16:53:26 -0400

    adding to gitignore

--- data/simulated_reads/predict.mk
+++
@@ -1,6 +0,0 @@
-PRED=python $(TS_HOME)/bin/predict.py
-PRED_ARGS=--write-all
-
-%.pred: %.ts
-	$(PRED) --in $< --out $@ $(PRED_ARGS) --verbose --subsampling-fraction=0.3 --plot-all --subsampling-series 0.05,0.1,0.2,0.3,0.4,0.5,1.0 --subsampling-repl=5
-	$(PRED) --version > $@/pred_version
