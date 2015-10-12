41d6a843550400b1b0bde3fd691f9f156082e91d
BenLangmead <langmea@cs.jhu.edu>
2015-10-12 14:09:54 -0400

    switching Makefiles over to new qsim regime

--- data/simulated_reads/hhpc_out.py
+++ data/simulated_reads/hhpc_out.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+"""
+python hhpc_out.py dry
+
+for dry run: write scripts but doesn't qsub them
+
+python hhpc_out.py dry
+
+for normal run: write scripts and also qsub them
+"""
+
 import os
 import sys
 import time
@@ -7,7 +17,7 @@ import re
 
 
 idx = 0
-tss_re = re.compile('tss[_a-zA-Z01-9]*:.*')
+re_out = re.compile('^out_[_a-zA-Z01-9]*:.*')
 mem_gb = 8
 
 
@@ -25,18 +35,18 @@ def mkdir_quiet(dr):
 def handle_dir(dirname, dry_run=True):
     global idx
     with open(os.path.join(dirname, 'Makefile')) as fh:
-        in_reads = False
+        in_out = False
         for ln in fh:
             if ln[0] == '#':
                 continue
-            if tss_re.match(ln):
-                in_reads = True
-            elif in_reads:
+            if re_out.match(ln):
+                in_out = True
+            elif in_out:
                 if len(ln.rstrip()) == 0:
-                    in_reads = False
+                    in_out = False
                 else:
                     target = ln.split()[0]
-                    print >> sys.stderr, '  Found a read file: %s' % target
+                    print >> sys.stderr, '  Found a .out target: %s' % target
                     target_full = os.path.join(dirname, target)
                     if os.path.exists(os.path.join(target_full, 'DONE')):
                         print >> sys.stderr, '  Skipping target %s because of DONE' % target
@@ -49,7 +59,7 @@ def handle_dir(dirname, dry_run=True):
                         my_mem_gb = 12
                     if '_snap' in target_full:
                         my_mem_gb = 64
-                    if '_10M.' in target_full:
+                    if '_50M.' in target_full:
                         my_mem_gb = int(my_mem_gb * 1.5)
                     pbs_lns = list()
                     pbs_lns.append('#PBS -q batch')
@@ -63,10 +73,6 @@ def handle_dir(dirname, dry_run=True):
                     pbs_lns.append('export TMPDIR=/scratch1/langmead-fs1/temp/langmead')
                     pbs_lns.append('cd %s' % os.path.abspath(dirname))
                     pbs_lns.append('if make %s ; then touch %s/DONE ; fi' % (target, target))
-                    #qsub_dir = '.tss_qsubs'
-                    #mkdir_quiet(qsub_dir)
-                    #cur_dir = os.getcwd()
-                    #os.chdir(qsub_dir)
                     qsub_basename = '.' + target + '.sh'
                     qsub_fullname = os.path.join(dirname, qsub_basename)
                     with open(qsub_fullname, 'w') as ofh:
@@ -75,8 +81,7 @@ def handle_dir(dirname, dry_run=True):
                     print 'pushd %s && qsub %s && popd' % (dirname, qsub_basename)
                     if not dry_run:
                         os.system('cd %s && qsub %s' % (dirname, qsub_basename))
-                        time.sleep(0.2)
-                    #os.chdir(cur_dir)
+                        time.sleep(0.5)
 
 
 def go():
@@ -89,6 +94,9 @@ def go():
     for dirname, dirs, files in os.walk('.'):
         if 'Makefile' in files:
             print >> sys.stderr, 'Found a Makefile: %s' % (os.path.join(dirname, 'Makefile'))
-            handle_dir(dirname, dry_run=(len(sys.argv) > 1))
+            handle_dir(dirname, dry_run=sys.argv[1] == 'dry')
 
-go()
+if len(sys.argv) == 1:
+    print "pass argument 'dry' for dry run, or 'wet' for normal run"
+else:
+    go()
