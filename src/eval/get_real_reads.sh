aad1bbec33aaa44e3dac61d112f657871edb2bb0
BenLangmead <langmea@cs.jhu.edu>
2016-01-12 22:43:57 -0500

    adding some zea mays reads

--- src/eval/get_real_reads.sh
+++ src/eval/get_real_reads.sh
@@ -9,6 +9,14 @@ for d in ERR050082 ERR050083 ; do
     pref=`echo ${d} | sed 's/\(......\).*/\1/'`
 
     for m in 1 2 ; do
-        curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${pref}/${d}/${d}_${m}.fastq.gz | gzip -dc > ${d}_${m}.fastq
+        if [ ! -f "${d}_${m}.fastq" ] ; then
+            curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${pref}/${d}/${d}_${m}.fastq.gz | gzip -dc > ${d}_${m}.fastq
+        fi
     done
 done
+
+for m in 1 2 ; do
+    curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR159/009/SRR1592709/SRR1592709_${m}.fastq.gz | gzip -dc > SRR1592709_${m}.fastq
+done
+
+# Zea mays reads
