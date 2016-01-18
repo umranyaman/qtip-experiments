67d9bd3bbe639e35ab8c4b832a1f19598af32bfe
BenLangmead <langmea@cs.jhu.edu>
2016-01-17 22:04:09 -0500

    Make very sensitive runs multi-threaded and not wrapped

--- src/eval/sbatch_all.sh
+++ src/eval/sbatch_all.sh
@@ -2,50 +2,44 @@
 
 for dat in ERR050082_1 ERR050083_1 ; do
 for pe in unp pair ; do
-for ext in bt2vs.${pe}.sam bt2vsl.${pe}.sam bwa.${pe}.sam bt2.${pe}.sam ; do
+
+for ext in bt2vs.${pe}.sam bt2vsl.${pe}.sam ; do
 
 cat >.${dat}.${ext} <<EOF
 #!/bin/sh
 #SBATCH --partition=shared
-#SBATCH --time=48:00:00
+#SBATCH --time=8:00:00
+#SBATCH --mem=12G
+#SBATCH --cpus-per-task=16
+/usr/bin/time -v make ${dat}.${ext}
+EOF
+echo "sbatch .${dat}.${ext}"
+
+done
+
+for ext in bwa.${pe}.sam bt2.${pe}.sam ; do
+
+cat >.${dat}.${ext} <<EOF
+#!/bin/sh
+#SBATCH --partition=shared
+#SBATCH --time=24:00:00
 #SBATCH --mem=12G
 /usr/bin/time -v make ${dat}.${ext}
 EOF
 echo "sbatch .${dat}.${ext}"
 
-: <<'END'
-cat >.${dat}.${ext}.plain <<EOF
-#!/bin/sh
-#SBATCH --partition=shared
-#SBATCH --time=48:00:00
-#SBATCH --mem=12G
-/usr/bin/time -v make ${dat}.${ext}.plain
-EOF
-echo "sbatch .${dat}.${ext}.plain"
-END
-
 done
+
 for ext in snap.${pe}.sam ; do
 
 cat >.${dat}.${ext} <<EOF
 #!/bin/sh
 #SBATCH --partition=shared
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=48G
 /usr/bin/time -v make ${dat}.${ext}
 EOF
 echo "sbatch .${dat}.${ext}"
-
-: <<'END'
-cat >.${dat}.${ext}.plain <<EOF
-#!/bin/sh
-#SBATCH --partition=shared
-#SBATCH --time=48:00:00
-#SBATCH --mem=48G
-/usr/bin/time -v make ${dat}.${ext}.plain
-EOF
-echo "sbatch .${dat}.${ext}.plain"
-END
 
 done
 
