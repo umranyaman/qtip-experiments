17a1651042fb3b678a1d31af446d73d8a1281153
BenLangmead <langmea@cs.jhu.edu>
2016-01-17 20:30:42 -0500

    better peak memory measures for tabulate

--- src/eval/tabulate.py
+++ src/eval/tabulate.py
@@ -18,7 +18,7 @@ for fn in glob.glob('slurm-*.out'):
             continue  # skip trivial job target was already up to date
         if ln.split()[0] == 'python':
             nsam += 1
-            name, maxmss, wallcl, tandal, tandpa = None, 0, 0, 0, 0
+            name, maxmss, wallcl, tandal, tandpa, wrappeak, childpeak = None, 0, 0, 0, 0, 0, 0
             while True:
                 ln = fh.readline()
                 if len(ln) == 0:
@@ -36,10 +36,19 @@ for fn in glob.glob('slurm-*.out'):
                     tandal = float(ln.split()[-1])
                 if 'INFO:Parsing tandem alignments' in ln:
                     tandpa = float(ln.split()[-1])
+                if 'INFO:Peak memory usage (RSS) of Python wrapper' in ln:
+                    wrappeak = ln.split()[-1]
+                    assert wrappeak[-2:] == 'GB'
+                    wrappeak = float(wrappeak[:-2]) * 1024 * 1024 * 1024
+                if 'INFO:Peak memory usage (RSS) of children' in ln:
+                    childpeak = ln.split()[-1]
+                    assert childpeak[-2:] == 'GB'
+                    childpeak = float(childpeak[:-2]) * 1024 * 1024 * 1024
 
             sam_names[name] += 1
-            #assert maxmss > 0
             tab_wrapped[name]['maxmss'] = maxmss
+            tab_wrapped[name]['wrappeak'] = wrappeak
+            tab_wrapped[name]['childpeak'] = childpeak
             tab_wrapped[name]['wallcl'] = wallcl
             tab_wrapped[name]['tandal'] = tandal
             tab_wrapped[name]['tandpa'] = tandpa
@@ -64,7 +73,6 @@ for fn in glob.glob('slurm-*.out'):
                     wallcl = toks[0]*60*60 + toks[1]*60 + toks[2]
 
             plain_names[name] += 1
-            #assert maxmss > 0
             tab_plain[name]['maxmss'] = maxmss
             tab_plain[name]['wallcl'] = wallcl
             to_slurm_plain[name] = fn
@@ -80,20 +88,18 @@ for k, v in sorted(plain_names.items()):
     print('  %s: %d' % (k, v))
 
 for k in sorted(sam_names.keys()):
-    maxmss = tab_wrapped[k]['maxmss']
-    maxmss_plain = tab_plain[k]['maxmss']
     wallcl = tab_wrapped[k]['wallcl']
     wallcl_plain = tab_plain[k]['wallcl']
-    diff = maxmss - maxmss_plain
-    diff_pct = 0 if maxmss_plain == 0 else (100.0 * float(diff) / maxmss_plain)
-    diff_pct = ('+' if diff_pct >= 0 else '') + ('%0.2f' % diff_pct)
     diffcl = wallcl - wallcl_plain
     diffcl_pct = 0 if wallcl_plain == 0 else (100.0 * float(diffcl) / wallcl_plain)
     diffcl_pct = ('+' if diffcl_pct >= 0 else '') + ('%0.2f' % diffcl_pct)
     difftandem = tab_wrapped[k]['tandal'] + tab_wrapped[k]['tandpa']
     difftandem_pct = 0 if wallcl_plain == 0 else (100.0 * float(difftandem) / wallcl_plain)
     difftandem_pct = ('+' if difftandem_pct >= 0 else '') + ('%0.2f' % difftandem_pct)
-    print('%s: %d %d %s tandem:%s %d %d %s %s %s' % (k, wallcl, wallcl_plain, diffcl_pct,
-                                                     difftandem_pct,
-                                                     maxmss_plain, maxmss, diff_pct,
+    wrappeak = tab_wrapped[name]['wrappeak']
+    childpeak = tab_wrapped[name]['childpeak']
+    wrappct = 0 if childpeak == 0 else (wrappeak * 100.0 / childpeak)
+    wrappct = ('+' if wrappct >= 0 else '') + ('%0.2f' % wrappct)
+    print('%s: %d,%d,%s,%s %.0f,%.0f,%s %s %s' % (k, wallcl, wallcl_plain, diffcl_pct, difftandem_pct,
+                                                     wrappeak, childpeak, wrappct,
                                                      to_slurm_plain[k], to_slurm_wrapped[k]))
