dc868ebf4ec26ce3e4e5b03ed239b56124b789ff
Ben <langmea@cs.jhu.edu>
2016-03-18 09:31:23 -0400

    remove mosaik

--- experiments/refs/submit_index.sh
+++ experiments/refs/submit_index.sh
@@ -34,19 +34,6 @@ mv $TS_REFS/hg19.fa.* .
 EOF
 echo qsub .hg19.bwa.sh
 
-cat > .hg19.mosaik.sh <<EOF
-#PBS -q batch
-#PBS -l walltime=10:00:00
-#PBS -j n
-#PBS -l pmem=8gb
-#PBS -l vmem=8gb
-#PBS -l pvmem=8gb
-#PBS -l mem=8gb
-cd $PWD
-$TS_HOME/software/mosaik/MosaikBuild -fr $TS_REFS/hg19.fa -oa hg19.fa.mosaik
-EOF
-echo qsub .hg19.mosaik.sh
-
 cat > .hg19.snap.sh <<EOF
 #PBS -q batch
 #PBS -l walltime=1:00:00
@@ -92,19 +79,6 @@ mv $TS_REFS/mm10.fa.* .
 EOF
 echo qsub .mm10.bwa.sh
 
-cat > .mm10.mosaik.sh <<EOF
-#PBS -q batch
-#PBS -l walltime=10:00:00
-#PBS -j n
-#PBS -l pmem=8gb
-#PBS -l vmem=8gb
-#PBS -l pvmem=8gb
-#PBS -l mem=8gb
-cd $PWD
-$TS_HOME/software/mosaik/MosaikBuild -fr $TS_REFS/mm10.fa -oa mm10.fa.mosaik
-EOF
-echo qsub .mm10.mosaik.sh
-
 cat > .mm10.snap.sh <<EOF
 #PBS -q batch
 #PBS -l walltime=1:00:00
@@ -149,19 +123,6 @@ bwa index $TS_REFS/zm_AGPv3.fa
 mv $TS_REFS/zm_AGPv3.fa.* .
 EOF
 echo qsub .zm_AGPv3.bwa.sh
-
-cat > .zm_AGPv3.mosaik.sh <<EOF
-#PBS -q batch
-#PBS -l walltime=10:00:00
-#PBS -j n
-#PBS -l pmem=8gb
-#PBS -l vmem=8gb
-#PBS -l pvmem=8gb
-#PBS -l mem=8gb
-cd $PWD
-$TS_HOME/software/mosaik/MosaikBuild -fr $TS_REFS/zm_AGPv3.fa -oa zm_AGPv3.fa.mosaik
-EOF
-echo qsub .zm_AGPv3.mosaik.sh
 
 cat > .zm_AGPv3.snap.sh <<EOF
 #PBS -q batch
