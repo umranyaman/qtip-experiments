fee14a1d9e163b538bca4101eaf3b56f2bd4e40a
BenLangmead <langmea@cs.jhu.edu>
2016-06-25 13:36:29 -0400

    dirname -> dir

--- experiments/simulated_reads/bwamemts.mk
+++ experiments/simulated_reads/bwamemts.mk
@@ -6,33 +6,33 @@ BWA_TS_ARGS=--write-orig-mapq
 define bwamemts
 
 r0_$1_%.$8/DONE: r0_%.fq.gz
-	mkdir -p $$(dirname $$(@)).temp
+	mkdir -p $$(dir $$(@)).temp
 	$$(QSIM) --ref $6 \
 	       --bwa-exe $$(BWA) --aligner=bwa-mem \
 	       --index $7 \
 	       --sim-unp-min $2 \
 	       $$(BWA_TS_ARGS) $$(TS_ARGS) $4 \
-	       --output-directory $$(dirname $$(@))\
-	       --temp-directory $$(dirname $$(@)).temp \
+	       --output-directory $$(dir $$(@))\
+	       --temp-directory $$(dir $$(@)).temp \
 	       --U $$< \
 	       -- $5 $$(BWA_ARGS) $$(BWA_EXTRA_ARGS) -t 1
-	-$$(BWA) > $$(dirname $$(@))/bwa_version 2>&1
-	-$$(QSIM) --version > $$(dirname $$(@))/qsim_version
+	-$$(BWA) > $$(dir $$(@))/bwa_version 2>&1
+	-$$(QSIM) --version > $$(dir $$(@))/qsim_version
 	touch $$(@)
 
 r12_$1_%.$8/DONE: r1_%.fq.gz
-	mkdir -p $$(dirname $$(@)).temp
+	mkdir -p $$(dir $$(@)).temp
 	$$(QSIM) --ref $6 \
 	       --bwa-exe $$(BWA) --aligner=bwa-mem \
 	       --index $7 \
 	       --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 \
 	       $$(BWA_TS_ARGS) $$(TS_ARGS) $4 \
-	       --output-directory $$(dirname $$(@)) \
-	       --temp-directory $$(dirname $$(@)).temp \
+	       --output-directory $$(dir $$(@)) \
+	       --temp-directory $$(dir $$(@)).temp \
 	       --m1 $$< --m2 $$(<:r1_%=r2_%) \
 	       -- $5 $$(BWA_ARGS) $$(BWA_EXTRA_ARGS) -t 1
-	-$$(BWA) > $$(dirname $$(@))/bwa_version 2>&1
-	-$$(QSIM) --version > $$(dirname $$(@))/qsim_version
+	-$$(BWA) > $$(dir $$(@))/bwa_version 2>&1
+	-$$(QSIM) --version > $$(dir $$(@))/qsim_version
 	touch $$(@)
 
 endef
