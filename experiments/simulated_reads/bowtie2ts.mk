ffd4c814bbd5a8efda51bfb729c7ba77c1c826ac
BenLangmead <langmea@cs.jhu.edu>
2016-06-25 10:03:32 -0400

    attempt to fix macro for bowtie2

--- experiments/simulated_reads/bowtie2ts.mk
+++ experiments/simulated_reads/bowtie2ts.mk
@@ -6,33 +6,33 @@ BT2_QSIM_ARGS=--write-orig-mapq
 define bt2ts
 
 r0_$1_%.$8/DONE: r0_%.fq.gz
-	mkdir -p $$(dirname $$@).temp
+	mkdir -p r0_$1_%.$8.temp
 	$$(QSIM) --ref $6 \
 	       --bt2-exe $$(BOWTIE2) \
 	       --index $7 \
 	       --sim-unp-min $2 \
 	       $$(BT2_TS_ARGS) $$(TS_ARGS) $4 \
-	       --output-directory $$(dirname $$@) \
-	       --temp-directory $$(dirname $$@).temp \
+	       --output-directory r0_$1_%.$8 \
+	       --temp-directory r0_$1_%.$8.temp \
 	       --U $$< \
 	       -- $5 $$(BT2_ARGS) -p 1
-	-$$(BOWTIE2) --version > $$(dirname $$@)/bt2_version
-	-$$(QSIM) --version > $$(dirname $$@)/qsim_version
-	touch $$@
+	-$$(BOWTIE2) --version > r0_$1_%.$8/bt2_version
+	-$$(QSIM) --version > r0_$1_%.$8/qsim_version
+	touch r0_$1_%.$8/DONE
 
 r12_$1_%.$8/DONE: r1_%.fq.gz
-	mkdir -p $$(dirname $$@).temp
+	mkdir -p r12_$1_%.$8.temp
 	$$(QSIM) --ref $6 \
 	       --bt2-exe $$(BOWTIE2) \
 	       --index $7 \
 	       --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 \
 	       $$(BT2_TS_ARGS) $$(TS_ARGS) $4 \
-	       --output-directory $$(dirname $$@) \
-	       --temp-directory $$(dirname $$@).temp \
+	       --output-directory r12_$1_%.$8 \
+	       --temp-directory r12_$1_%.$8.temp \
 	       --m1 $$< --m2 $$(<:r1_%=r2_%) \
 	       -- $5 $$(BT2_ARGS) -p 1
-	-$$(BOWTIE2) --version > $$(dirname $$@)/bt2_version
-	-$$(QSIM) --version > $$(dirname $$@)/qsim_version
-	touch $$@
+	-$$(BOWTIE2) --version > r12_$1_%.$8/bt2_version
+	-$$(QSIM) --version > r12_$1_%.$8/qsim_version
+	touch r12_$1_%.$8/DONE
 
 endef
