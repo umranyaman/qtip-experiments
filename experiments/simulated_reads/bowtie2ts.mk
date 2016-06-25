b46d023e1796ac3f1009b17e55f709655ef6bb76
BenLangmead <langmea@cs.jhu.edu>
2016-06-25 13:39:56 -0400

    changing to shell dirname

--- experiments/simulated_reads/bowtie2ts.mk
+++ experiments/simulated_reads/bowtie2ts.mk
@@ -6,33 +6,33 @@ BT2_QSIM_ARGS=--write-orig-mapq
 define bt2ts
 
 r0_$1_%.$8/DONE: r0_%.fq.gz
-	mkdir -p $$(dirname $$(@)).temp
+	mkdir -p $$(shell dirname $$(@)).temp
 	$$(QSIM) --ref $6 \
 	       --bt2-exe $$(BOWTIE2) \
 	       --index $7 \
 	       --sim-unp-min $2 \
 	       $$(BT2_TS_ARGS) $$(TS_ARGS) $4 \
-	       --output-directory $$(dirname $$(@)) \
-	       --temp-directory $$(dirname $$(@)).temp \
+	       --output-directory $$(shell dirname $$(@)) \
+	       --temp-directory $$(shell dirname $$(@)).temp \
 	       --U $$< \
 	       -- $5 $$(BT2_ARGS) -p 1
-	-$$(BOWTIE2) --version > $$(dirname $$(@))/bt2_version
-	-$$(QSIM) --version > $$(dirname $$(@))/qsim_version
+	-$$(BOWTIE2) --version > $$(shell dirname $$(@))/bt2_version
+	-$$(QSIM) --version > $$(shell dirname $$(@))/qsim_version
 	touch $$(@)
 
 r12_$1_%.$8/DONE: r1_%.fq.gz
-	mkdir -p $$(dirname $$(@)).temp
+	mkdir -p $$(shell dirname $$(@)).temp
 	$$(QSIM) --ref $6 \
 	       --bt2-exe $$(BOWTIE2) \
 	       --index $7 \
 	       --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 \
 	       $$(BT2_TS_ARGS) $$(TS_ARGS) $4 \
-	       --output-directory $$(dirname $$(@)) \
-	       --temp-directory $$(dirname $$(@)).temp \
+	       --output-directory $$(shell dirname $$(@)) \
+	       --temp-directory $$(shell dirname $$(@)).temp \
 	       --m1 $$< --m2 $$(<:r1_%=r2_%) \
 	       -- $5 $$(BT2_ARGS) -p 1
-	-$$(BOWTIE2) --version > $$(dirname $$(@))/bt2_version
-	-$$(QSIM) --version > $$(dirname $$(@))/qsim_version
+	-$$(BOWTIE2) --version > $$(shell dirname $$(@))/bt2_version
+	-$$(QSIM) --version > $$(shell dirname $$(@))/qsim_version
 	touch $$(@)
 
 endef
--- experiments/simulated_reads/bwamemts.mk
+++ experiments/simulated_reads/bwamemts.mk
@@ -6,33 +6,33 @@ BWA_TS_ARGS=--write-orig-mapq
 define bwamemts
 
 r0_$1_%.$8/DONE: r0_%.fq.gz
-	mkdir -p $$(dir $$(@)).temp
+	mkdir -p $$(shell dirname $$(@)).temp
 	$$(QSIM) --ref $6 \
 	       --bwa-exe $$(BWA) --aligner=bwa-mem \
 	       --index $7 \
 	       --sim-unp-min $2 \
 	       $$(BWA_TS_ARGS) $$(TS_ARGS) $4 \
-	       --output-directory $$(dir $$(@))\
-	       --temp-directory $$(dir $$(@)).temp \
+	       --output-directory $$(shell dirname $$(@))\
+	       --temp-directory $$(shell dirname $$(@)).temp \
 	       --U $$< \
 	       -- $5 $$(BWA_ARGS) $$(BWA_EXTRA_ARGS) -t 1
-	-$$(BWA) > $$(dir $$(@))/bwa_version 2>&1
-	-$$(QSIM) --version > $$(dir $$(@))/qsim_version
+	-$$(BWA) > $$(shell dirname $$(@))/bwa_version 2>&1
+	-$$(QSIM) --version > $$(shell dirname $$(@))/qsim_version
 	touch $$(@)
 
 r12_$1_%.$8/DONE: r1_%.fq.gz
-	mkdir -p $$(dir $$(@)).temp
+	mkdir -p $$(shell dirname $$(@)).temp
 	$$(QSIM) --ref $6 \
 	       --bwa-exe $$(BWA) --aligner=bwa-mem \
 	       --index $7 \
 	       --sim-conc-min $2 --sim-disc-min $3 --sim-bad-end-min $3 \
 	       $$(BWA_TS_ARGS) $$(TS_ARGS) $4 \
-	       --output-directory $$(dir $$(@)) \
-	       --temp-directory $$(dir $$(@)).temp \
+	       --output-directory $$(shell dirname $$(@)) \
+	       --temp-directory $$(shell dirname $$(@)).temp \
 	       --m1 $$< --m2 $$(<:r1_%=r2_%) \
 	       -- $5 $$(BWA_ARGS) $$(BWA_EXTRA_ARGS) -t 1
-	-$$(BWA) > $$(dir $$(@))/bwa_version 2>&1
-	-$$(QSIM) --version > $$(dir $$(@))/qsim_version
+	-$$(BWA) > $$(shell dirname $$(@))/bwa_version 2>&1
+	-$$(QSIM) --version > $$(shell dirname $$(@))/qsim_version
 	touch $$(@)
 
 endef
