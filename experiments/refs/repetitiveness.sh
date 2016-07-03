c16b67657c570ce4dd1cad9d42927c80028f69b8
BenLangmead <langmea@cs.jhu.edu>
2016-07-02 20:36:17 -0400

    download the file

--- experiments/refs/repetitiveness.sh
+++ experiments/refs/repetitiveness.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-curl http://hgdownload.cse.ucsc.edu/goldenPath/mm10/bigZips/chromFa.tar.gz \
-    gzip -dc | grep -av '^>' | tee >(tr -cd ACGTN | wc -c | tee mm10_upper.txt | xargs echo "mm10 upper") | \
-                                     tr -cd acgtn | wc -c | tee mm10_lower.txt | xargs echo "mm10 lower"
+mkdir -p mm10
+cd mm10
+wget http://hgdownload.cse.ucsc.edu/goldenpath/mm10/bigzips/chromfa.tar.gz
+tar xvf chromfa.tar.gz
+cat *.fa | grep -av '^>' | tee >(tr -cd acgtn | wc -c | tee mm10_upper.txt | xargs echo "mm10 upper") | \
+                                 tr -cd acgtn | wc -c | tee mm10_lower.txt | xargs echo "mm10 lower"
+cd ..
+rm -rf mm10
 
-curl http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz \
-    gzip -dc | grep -av '^>' | tee >(tr -cd ACGTN | wc -c | tee hg19_upper.txt | xargs echo "hg19 upper") | \
-                                     tr -cd acgtn | wc -c | tee hg19_lower.txt | xargs echo "hg19 lower"
+mkdir -p hg19
+cd hg19
+wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/bigzips/chromfa.tar.gz
+tar xvf chromfa.tar.gz
+cat *.fa | grep -av '^>' | tee >(tr -cd acgtn | wc -c | tee mm10_upper.txt | xargs echo "hg19 upper") | \
+                                 tr -cd acgtn | wc -c | tee mm10_lower.txt | xargs echo "hg19 lower"
+cd ..
+rm -rf hg19
