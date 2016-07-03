e9fdd1f92427ae9488b4c4291a7b06887dcd2d7e
BenLangmead <langmea@cs.jhu.edu>
2016-07-02 20:40:50 -0400

    last typo

--- experiments/refs/repetitiveness.sh
+++ experiments/refs/repetitiveness.sh
@@ -3,7 +3,7 @@
 mkdir -p mm10
 cd mm10
 wget http://hgdownload.cse.ucsc.edu/goldenPath/mm10/bigZips/chromFa.tar.gz
-tar xvf chromfa.tar.gz
+tar xvf chromFa.tar.gz
 cat *.fa | grep -av '^>' | tee >(tr -cd acgtn | wc -c | tee ../mm10_upper.txt | xargs echo "mm10 upper") | \
                                  tr -cd acgtn | wc -c | tee ../mm10_lower.txt | xargs echo "mm10 lower"
 cd ..
@@ -12,7 +12,7 @@ rm -rf mm10
 mkdir -p hg19
 cd hg19
 wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz
-tar xvf chromfa.tar.gz
+tar xvf chromFa.tar.gz
 cat *.fa | grep -av '^>' | tee >(tr -cd acgtn | wc -c | tee ../hg19_upper.txt | xargs echo "hg19 upper") | \
                                  tr -cd acgtn | wc -c | tee ../hg19_lower.txt | xargs echo "hg19 lower"
 cd ..
