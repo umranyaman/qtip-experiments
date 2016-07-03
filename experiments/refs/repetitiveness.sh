4bd261ed6f019bf83d2e12db3b33b894c7080694
BenLangmead <langmea@cs.jhu.edu>
2016-07-02 20:39:32 -0400

    fixing output filenames

--- experiments/refs/repetitiveness.sh
+++ experiments/refs/repetitiveness.sh
@@ -4,8 +4,8 @@ mkdir -p mm10
 cd mm10
 wget http://hgdownload.cse.ucsc.edu/goldenPath/mm10/bigZips/chromFa.tar.gz
 tar xvf chromfa.tar.gz
-cat *.fa | grep -av '^>' | tee >(tr -cd acgtn | wc -c | tee mm10_upper.txt | xargs echo "mm10 upper") | \
-                                 tr -cd acgtn | wc -c | tee mm10_lower.txt | xargs echo "mm10 lower"
+cat *.fa | grep -av '^>' | tee >(tr -cd acgtn | wc -c | tee ../mm10_upper.txt | xargs echo "mm10 upper") | \
+                                 tr -cd acgtn | wc -c | tee ../mm10_lower.txt | xargs echo "mm10 lower"
 cd ..
 rm -rf mm10
 
@@ -13,7 +13,7 @@ mkdir -p hg19
 cd hg19
 wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz
 tar xvf chromfa.tar.gz
-cat *.fa | grep -av '^>' | tee >(tr -cd acgtn | wc -c | tee mm10_upper.txt | xargs echo "hg19 upper") | \
-                                 tr -cd acgtn | wc -c | tee mm10_lower.txt | xargs echo "hg19 lower"
+cat *.fa | grep -av '^>' | tee >(tr -cd acgtn | wc -c | tee ../hg19_upper.txt | xargs echo "hg19 upper") | \
+                                 tr -cd acgtn | wc -c | tee ../hg19_lower.txt | xargs echo "hg19 lower"
 cd ..
 rm -rf hg19
