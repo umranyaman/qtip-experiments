3f52c17403f2700e10851be0b18f0d2d6d606034
BenLangmead <langmea@cs.jhu.edu>
2016-09-30 14:56:33 -0400

    fixes silly extension typo

--- experiments/refs/get_refs.sh
+++ experiments/refs/get_refs.sh
@@ -17,7 +17,7 @@ GRCm38_URL=ftp://ftp.ensembl.org/pub/release-75/fasta/mus_musculus/dna/$GRCm38_F
 
 ftp://ftp.ensemblgenomes.org/pub/plants/release-32/fasta/zea_mays/dna/Zea_mays.AGPv4.dna.toplevel.fa.gz
 
-ZM_FA=Zea_mays.AGPv4.dna.toplevel.fa.gz
+ZM_FA=Zea_mays.AGPv4.dna.toplevel.fa
 ZM_URL=ftp://ftp.ensemblgenomes.org/pub/plants/release-32/fasta/zea_mays/dna/$ZM_FA.gz
 
 if [ ! -f $GRCh38_FA ] ; then
