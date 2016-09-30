6a4e0cc354756c02b0d845768f91bcbd0caf16bc
BenLangmead <langmea@cs.jhu.edu>
2016-09-30 13:12:50 -0400

    updating human and corn

--- experiments/refs/get_refs.sh
+++ experiments/refs/get_refs.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
 
-GRCh37_FA=Homo_sapiens.GRCh37.75.dna.primary_assembly.fa
-GRCh37_URL=ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/$GRCh37_FA.gz
+# Download reference genomes and remove contigs too small for Mason
+
+GRCh38_FA=Homo_sapiens.GRCh38.dna.primary_assembly.fa
+GRCh38_URL=ftp://ftp.ensembl.org/pub/release-85/fasta/homo_sapiens/dna/$GRCh38_FA.gz
 
 GRCm38_FA=Mus_musculus.GRCm38.75.dna.primary_assembly.fa
 GRCm38_URL=ftp://ftp.ensembl.org/pub/release-75/fasta/mus_musculus/dna/$GRCm38_FA.gz
 
-ZM_FA=Zea_mays.AGPv3.21.dna.genome.fa
-ZM_URL=ftp://ftp.ensemblgenomes.org/pub/plants/release-21/fasta/zea_mays/dna/$ZM_FA.gz
+# Note: there appears to be an AGPv4 assembly, but it is not yet published, has
+# a Toronto-agreement-style embargo, and the Ensembl website says "The
+# underlying assembly and final set of annotations may change until the data
+# has been accepted by GenBank."  So I'm laying off it for now.
 
-if [ ! -f $GRCh37_FA ] ; then
-    rm -f $GRCh37_FA.gz
-    wget $GRCh37_URL
-    gzip -dc $GRCh37_FA.gz | python remove_short.py $GRCh37_FA.short > $GRCh37_FA
-    samtools faidx $GRCh37_FA
+# http://plants.ensembl.org/Zea_mays/Info/Index
+
+ftp://ftp.ensemblgenomes.org/pub/plants/release-32/fasta/zea_mays/dna/Zea_mays.AGPv4.dna.toplevel.fa.gz
+
+ZM_FA=Zea_mays.AGPv4.dna.toplevel.fa.gz
+ZM_URL=ftp://ftp.ensemblgenomes.org/pub/plants/release-32/fasta/zea_mays/dna/$ZM_FA.gz
+
+if [ ! -f $GRCh38_FA ] ; then
+    rm -f $GRCh38_FA.gz
+    wget $GRCh38_URL
+    gzip -dc $GRCh38_FA.gz | python remove_short.py $GRCh38_FA.short > $GRCh38_FA
+    samtools faidx $GRCh38_FA
 fi
-ln -s -f $GRCh37_FA hg19.fa
-ln -s -f $GRCh37_FA.fai hg19.fa.fai
+ln -s -f $GRCh38_FA hg19.fa
+ln -s -f $GRCh38_FA.fai hg19.fa.fai
 
 if [ ! -f $GRCm38_FA ] ; then
     rm -f $GRCm38_FA.gz
@@ -33,5 +44,5 @@ if [ ! -f $ZM_FA ] ; then
     gzip -dc $ZM_FA.gz | python remove_short.py $ZM_FA.short > $ZM_FA
     samtools faidx $ZM_FA
 fi
-ln -s -f $ZM_FA zm_AGPv3.fa
-ln -s -f $ZM_FA.fai zm_AGPv3.fa.fai
+ln -s -f $ZM_FA zm_AGPv4.fa
+ln -s -f $ZM_FA.fai zm_AGPv4.fa.fai
