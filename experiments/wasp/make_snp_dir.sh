9bad8d50f2d47456b84d5354136f832313191c0f
Ben Langmead <ben.langmead@gmail.com>
2016-11-22 22:13:01 -0500

    add scripts for preparing bam and running wasp

--- experiments/wasp/make_snp_dir.sh
+++ experiments/wasp/make_snp_dir.sh
@@ -10,5 +10,5 @@ ENSEMBL_REL=85
 URL=ftp://ftp.ensembl.org/pub/release-${ENSEMBL_REL}/variation/vcf/homo_sapiens/Homo_sapiens.vcf.gz
 
 mkdir -p snps
-curl ${URL} | gzip -dc | awk '$1 !~ /^#/ && length($4) == 1 && length($5) == 1 && $4 != $5 {print $2,$4,$5 > "snps/"$1".snps.txt"}'
+curl ${URL} | gzip -dc | awk '$1 !~ /^#/ && length($4) == 1 && length($5) == 1 && $4 != $5 && $4 != "." && $5 != "." {print $2,$4,$5 > "snps/"$1".snps.txt"}'
 gzip snps/*.txt
