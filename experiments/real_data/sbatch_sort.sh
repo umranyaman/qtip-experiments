dce09fe119a9924618c4de39c21dc2973e17da97
BenLangmead <langmea@cs.jhu.edu>
2016-11-25 15:30:19 -0500

    fixing a couple typos

--- experiments/real_data/sbatch_sort.sh
+++ experiments/real_data/sbatch_sort.sh
@@ -14,17 +14,17 @@ for dat in ERR050082_1 ERR050083_1 ; do
 for pe in unp pair ; do
 for ext in bwa bt2 snap ; do
 
-cat >.${dat}.${pe}.${ext}.sort.sh <<EOF
+cat >.${dat}.${ext}.${pe}.sort.sh <<EOF
 #!/bin/sh
 ${PART1}
 ${PART2}
 #SBATCH --time=8:00:00
 #SBATCH --mem=4G
-if samtools sort -o ${dat}.${pe}.${ext}.sam.bam ${dat}.${pe}.${ext}.sorted.bam ; then
-    rm -f ${dat}.${pe}.${ext}.sam.bam
+if samtools sort -o ${dat}.${ext}.${pe}.sorted.bam ${dat}.${ext}.${pe}.sam.bam ; then
+    rm -f ${dat}.${ext}.${pe}.sam.bam
 fi
 EOF
-echo "sbatch .${dat}.${pe}.${ext}.sort.sh"
+echo "sbatch .${dat}.${ext}.${pe}.sort.sh"
 
 done
 done
