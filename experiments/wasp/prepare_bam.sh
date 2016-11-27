f3a032df141030861898de9a083aa2ed6fa4232b
Ben Langmead <ben.langmead@gmail.com>
2016-11-27 13:06:27 -0500

    removing old scripts

--- experiments/wasp/prepare_bam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-#SBATCH
-#SBATCH --job-name=PrepareSam
-#SBATCH --time=24:0:0
-#SBATCH --partition=shared
-#SBATCH --nodes=1
-
-samtools sort ../real_data/ERR050082_1.bt2.unp.sam -o ERR050082_1.bt2.unp.sorted.bam
