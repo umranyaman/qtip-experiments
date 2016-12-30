ec736c2381bb7ef2988ea6e76582b694f2561b4c
BenLangmead <langmea@cs.jhu.edu>
2016-12-30 12:30:46 -0500

    more descriptive job name and output file names

--- experiments/real_data/sbatch_correctish.sh
+++ experiments/real_data/sbatch_correctish.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Does this need sbatch_sort.sh to have run first?
+
+# Run after sbatch_align.sh and before sbatch_align_correctish.sh
+
 if [ "$1" = "scavenger" ] ; then
 PART1="#SBATCH --partition=scavenger"
 PART2="#SBATCH --qos=scavenger"
@@ -12,6 +16,9 @@ for dat in ERR050082 ERR050083 ; do
 
 cat >.${dat}.correctish.sh <<EOF
 #!/bin/sh
+#SBATCH --job-name=correctish
+#SBATCH --output=correctish-%A.out
+#SBATCH --error=correctish-%A.err
 ${PART1}
 ${PART2}
 #SBATCH --time=8:00:00
