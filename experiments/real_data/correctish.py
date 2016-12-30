25e019690c24b7d8872f9697b0f9f6df990f05c4
BenLangmead <langmea@cs.jhu.edu>
2016-12-30 12:26:18 -0500

    improves sanity checking

--- experiments/real_data/correctish.py
+++ experiments/real_data/correctish.py
@@ -12,10 +12,13 @@ Iterate through all the SAM files in tandem.  Take one of the results round-robi
 from __future__ import print_function
 import os
 import sys
+import gzip
 from operator import itemgetter
 
 
 def left_soft_clipped(cigar):
+    """ Given a cigar string, return the indicated amount of soft
+        clipping on the left side """
     n = 0
     for c in cigar:
         if str.isdigit(c):
@@ -24,36 +27,39 @@ def left_soft_clipped(cigar):
         else:
             if c == 'S':
                 return n
-            return 0
+            return 0  # leftmost op was something other than S
     return 0
 
 
 round_robin = 0
+
+
 def handle_lines(sam_linetoks, fastq_lines, ofhs):
     """
-	Example: 11_25006153_25006410_0:0:0_0:0:0_100_100_1_1/1
-	         ^ refid    ^ frag end (1-based) ^ len1  ^ flip
-	            ^ frag start (1-based)            ^ len2
-	                              (bunch of 0s)
 
 
-	fprintf(fpo[j], "@%s_%u_%u_%d:%d:%d_%d:%d:%d_%llx/%d\n", ks->name.s, ext_coor[0]+1, ext_coor[1]+1,
-	        n_err[0], n_sub[0], n_indel[0], n_err[1], n_sub[1], n_indel[1],
-	        (long long)ii, j==0? is_flip+1 : 2-is_flip);
+    Example: 11_25006153_25006410_0:0:0_0:0:0_100_100_1_1/1
+             ^ refid    ^ frag end (1-based) ^ len1  ^ flip
+                ^ frag start (1-based)            ^ len2
+                                  (bunch of 0s)
 
-	1. ks->name.s: ref name
-	2. ext_coor[0]+1: leftmost
-	3. ext_coor[1]+1: rightmost
-	4. n_err[0], 5. n_sub[0], 6. n_indel[0]
-	7. n_err[1], 8. n_sub[1], 9. n_indel[1]
-	10. ii is just the # of the pair
-	11. mate #
-	"""
+    fprintf(fpo[j], "@%s_%u_%u_%d:%d:%d_%d:%d:%d_%llx/%d\n", ks->name.s, ext_coor[0]+1, ext_coor[1]+1,
+            n_err[0], n_sub[0], n_indel[0], n_err[1], n_sub[1], n_indel[1],
+            (long long)ii, j==0? is_flip+1 : 2-is_flip);
+
+    1. ks->name.s: ref name
+    2. ext_coor[0]+1: leftmost
+    3. ext_coor[1]+1: rightmost
+    4. n_err[0], 5. n_sub[0], 6. n_indel[0]
+    7. n_err[1], 8. n_sub[1], 9. n_indel[1]
+    10. ii is just the # of the pair
+    11. mate #
+    """
     global round_robin
 
-    sam_linetoks1 = sam_linetoks[0][round_robin]
-    sam_linetoks2 = sam_linetoks[1][round_robin]
-    round_robin = (round_robin + 1) % len(sam_linetoks[0])
+    sam_linetoks1 = sam_linetoks[0][round_robin]  # one mate
+    sam_linetoks2 = sam_linetoks[1][round_robin]  # other mate from same aligner
+    round_robin = (round_robin + 1) % len(sam_linetoks[0])  # pick next round robin
 
     flags1, flags2 = int(sam_linetoks1[1]), int(sam_linetoks2[1])
     assert (flags1 & 1) != 0 and (flags2 & 1) != 0
@@ -73,21 +79,26 @@ def handle_lines(sam_linetoks, fastq_lines, ofhs):
     lpos = min(pos1, pos2)
     rpos = max(pos1 + len1 - 1, pos2 + len2 - 1)
 
-    name1 = '@%s_%d_%d_0:0:0_0:0:0_%d_%d_%s_1/1' % (refid, lpos, rpos, len1, len2, flip)
-    name2 = '@%s_%d_%d_0:0:0_0:0:0_%d_%d_%s_1/2' % (refid, lpos, rpos, len1, len2, flip)
-    ofhs[0].write('%s\n%s+\n%s' % (name1, fastq_lines[0][0], fastq_lines[0][1]))
-    ofhs[1].write('%s\n%s+\n%s' % (name2, fastq_lines[1][0], fastq_lines[1][1]))
-    return
+    name = '@%s_%d_%d_0:0:0_0:0:0_%d_%d_%s_1' % (refid, lpos, rpos, len1, len2, flip)
+    ofhs[0].write('%s/1\n%s+\n%s' % (name, fastq_lines[0][0], fastq_lines[0][1]))
+    ofhs[1].write('%s/2\n%s+\n%s' % (name, fastq_lines[1][0], fastq_lines[1][1]))
 
 
 def all_aligned(linetoks):
-    """ Must have aligned concordantly """
+    """ Given list of lists, where outer list has alignments from
+        different tools and inner has the line tokens for that tool,
+        return true iff flags indicate all alignments are paired and
+        concordant. """
     flags = map(int, map(itemgetter(1), linetoks))
     return all(map(lambda x: (x & 3) == 3 and (x & 4) == 0, flags))
 
 
 def next_pair_fastq(fastq_fhs):
+    """ Given list of filehandles for (hopefully parallel) FASTQ files,
+        return list of pairs, where each pair is seq and qual from one
+        file """
     fastq_lines = []
+    first_nm = None
     for fastq_fh in fastq_fhs:
         nm = fastq_fh.readline()
         seq = fastq_fh.readline()
@@ -97,11 +108,17 @@ def next_pair_fastq(fastq_fhs):
             raise RuntimeError('FASTQ ended before we expected')
         assert nm[0] == '@'
         assert nm2[0] == '+'
+        if first_nm is None:
+            first_nm = nm
+        elif nm != first_nm and nm[:-2] != first_nm[:-2]:
+            raise RuntimeError('Parallel FASTQ read names did not match:\n%s\n%s\n' % (nm, first_nm))
         fastq_lines.append((seq, qual))
     return fastq_lines
 
 
 def next_pair_sam(sam_fhs):
+    """ Return tokenized version of next non-secondary SAM record from several
+        sam files """
     sam_lines = []
     for fh in sam_fhs:
         ln = '@'
@@ -111,7 +128,7 @@ def next_pair_sam(sam_fhs):
             ln = fh.readline()
             if len(ln) == 0:
                 break
-            toks = ln.rstrip().split('\t')
+            toks = ln.split('\t')
         sam_lines.append(ln if len(ln) > 0 else None)
     if all(map(lambda x: x is None, sam_lines)):
         for fh in sam_fhs:
@@ -120,16 +137,31 @@ def next_pair_sam(sam_fhs):
     if any(map(lambda x: x is None, sam_lines)):
         raise RuntimeError('Files finished out of sync')
     linetoks = list(map(lambda x: x.rstrip().split('\t'), sam_lines))
+    for i in range(1, len(linetoks)):
+        if linetoks[i][0] != linetoks[0][0]:
+            raise RuntimeError('Read name mismatch between two SAM files: %s vs %s' %
+                               (linetoks[i][0], linetoks[0][0]))
     return False, linetoks
+
+
+def openex(fn, mode="rb"):
+    if fn.endswith(".gz"):
+        import gzip
+        return gzip.open(fn, mode)
+    else:
+        return open(fn, mode)
 
 
 def handle_sams(sam_fns, fastq_fns, output_fns):
     """
     Iterate through sams and FASTQs in lock step.
     """
-    sam_fhs = map(lambda x: open(x, 'rb'), sam_fns)
-    fastq_fhs = map(lambda x: open(x, 'rb'), fastq_fns)
+    sam_fhs = map(openex, sam_fns)
+    fastq_fhs = map(openex, fastq_fns)
     ln = None
+    nreads = 0
+    nreads_ival = 2
+    nreads_ival_frac = 1.2
     with open(output_fns[0], 'wb') as ofh1:
         with open(output_fns[1], 'wb') as ofh2:
             while True:
@@ -150,11 +182,16 @@ def handle_sams(sam_fns, fastq_fns, output_fns):
                     ln = len(sam_linetoks1)
                 fastq_lines = next_pair_fastq(fastq_fhs)
                 assert fastq_lines is not None
-                assert ln == len(sam_linetoks1), 'saved # alns %d != 1 this iteration %d' % (ln, len(sam_linetoks1))
-                assert ln == len(sam_linetoks2), 'saved # alns %d != 2 this iteration %d' % (ln, len(sam_linetoks2))
+                assert ln == len(sam_linetoks1)
+                assert ln == len(sam_linetoks2)
                 if all_aligned(sam_linetoks1) and all_aligned(sam_linetoks2):
+                    # Write corresponding FASTQ records to output files
                     handle_lines([sam_linetoks1, sam_linetoks2],
                                  fastq_lines, [ofh1, ofh2])
+                nreads += 1
+                if nreads == nreads_ival:
+                    print('Processed %d reads' % nreads)
+                    nreads_ival = int(nreads_ival * nreads_ival_frac) + 1
 
 
 def add_args(parser):
