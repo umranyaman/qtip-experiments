eb78fa4904a5aff10b6d3869cda6f877ffc39dac
BenLangmead <langmea@cs.jhu.edu>
2016-12-31 09:54:24 -0500

    added mouse

--- experiments/wasp/make_snp_dir.sh
+++ experiments/wasp/make_snp_dir.sh
@@ -9,6 +9,14 @@
 ENSEMBL_REL=85
 URL=ftp://ftp.ensembl.org/pub/release-${ENSEMBL_REL}/variation/vcf/homo_sapiens/Homo_sapiens.vcf.gz
 
-mkdir -p snps
-curl ${URL} | gzip -dc | awk '$1 !~ /^#/ && length($4) == 1 && length($5) == 1 && $4 != $5 && $4 != "." && $5 != "." {print $2,$4,$5 > "snps/"$1".snps.txt"}'
-gzip snps/*.txt
+mkdir -p human_ensembl85_snps
+curl ${URL} | gzip -dc | awk '$1 !~ /^#/ && length($4) == 1 && length($5) == 1 && $4 != $5 && $4 != "." && $5 != "." {print $2,$4,$5 > "human_ensembl85_snps/"$1".snps.txt"}'
+gzip human_ensembl85_snps/*.txt
+
+URL=ftp://ftp.ensembl.org/pub/release-${ENSEMBL_REL}/variation/vcf/mus_musculus/Mus_musculus.vcf.gz
+
+mkdir -p mouse_ensembl85_snps
+curl ${URL} | gzip -dc | awk '$1 !~ /^#/ && length($4) == 1 && length($5) == 1 && $4 != $5 && $4 != "." && $5 != "." {print $2,$4,$5 > "mouse_ensembl85_snps/"$1".snps.txt"}'
+gzip mouse_ensembl85_snps/*.txt
+
+# Ensembl does not have variation data for Zea Mays
