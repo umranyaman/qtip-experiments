f1d0ba50dca9189937188ac2491f60ee79f290e3
BenLangmead <langmea@cs.jhu.edu>
2016-12-31 15:21:24 -0500

    toward workable scripts for creating links and sorting

---
+++ experiments/wasp/links.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+echo "Making real-data SAM links..."
+for pa in unp pair ; do
+    for aln in bt2 bwa snap ; do
+        for dat in 2 3 ; do
+            FN="ERR05008${dat}_1.${aln}.${pa}.sam"
+            if [ ! -f "${FN}" ] ; then
+                echo "  * Making link to ../real_data/${FN}..."
+                ln -s -f ../real_data/${FN} ${FN}
+            else
+                echo "  Link to ../real_data/${FN} already present"
+            fi
+        done
+    done
+done
+
+echo "Making simulated data links..."
+for rdlen in 100 250 ; do
+    for genome in hg mm ; do
+        for aln in bt2s snap bwamem ; do
+            # Unpaired
+            FN="r0_${aln}_${genome}_mason_ill_${genome}_${rdlen}"
+            if [ ! -f "${FN}.trial0.sam" ] ; then
+                echo "  * Making link to ../simulated_data/various_genomes/${FN}.out/trial0/final.sam..."
+                ln -s -f ../simulated_data/various_genomes/${FN}.out/trial0/final.sam ${FN}.trial0.sam
+            else
+                echo "  Link to ../simulated_data/various_genomes/${FN}.out/trial0/final.sam already present"
+            fi
+
+            # Paired
+            FN="r12_${aln}${rdlen}_${genome}_mason_ill_${genome}_${rdlen}"
+            if [ ! -f "${FN}.trial0.sam" ] ; then
+                echo "  * Making link to ../simulated_data/various_genomes/${FN}.out/trial0/final.sam..."
+                ln -s -f ../simulated_data/various_genomes/${FN}.out/trial0/final.sam ${FN}.trial0.sam
+            else
+                echo "  Link to ../simulated_data/various_genomes/${FN}.out/trial0/final.sam already present"
+            fi
+        done
+    done
+done
---
+++ experiments/wasp/sbatch_sort.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+NTHREADS=8
+
+if [ "$1" = "scavenger" ] ; then
+PART1="#SBATCH --partition=scavenger"
+PART2="#SBATCH --qos=scavenger"
+else
+PART1="#SBATCH --partition=shared"
+PART2=""
+fi
+
+for pa in unp pair ; do
+    for aln in bt2 bwa snap ; do
+        for dat in 2 3 ; do
+            FN="ERR05008${dat}_1.${aln}.${pa}"
+            cat >.${FN}.sort.sh <<EOF
+#!/bin/sh
+#SBATCH --job-name=SortPreWasp
+#SBATCH --output ${FN}.out
+#SBATCH --error ${FN}.err
+#SBATCH --cpus-per-task=${NTHREADS}
+${PART1}
+${PART2}
+#SBATCH --time=8:00:00
+#SBATCH --mem=4G
+if sambamba sort -m 3G -t ${NTHREADS} -o ${FN} ${FN}.sam ; then
+    rm -f ${FN}.sam
+fi
+EOF
+            echo "sbatch .${FN}.sort.sh"
+
+        done
+    done
+done
+
+echo "Making simulated data links..."
+for rdlen in 100 250 ; do
+    for genome in hg mm ; do
+        for aln in bt2s snap bwamem ; do
+            # Unpaired
+            FN="r0_${aln}_${genome}_mason_ill_${genome}_${rdlen}"
+            cat >.${FN}.sort.sh <<EOF
+#!/bin/sh
+#SBATCH --job-name=SortPreWasp
+#SBATCH --output ${FN}.out
+#SBATCH --error ${FN}.err
+#SBATCH --cpus-per-task=${NTHREADS}
+${PART1}
+${PART2}
+#SBATCH --time=8:00:00
+#SBATCH --mem=4G
+if sambamba sort -m 3G -t ${NTHREADS} -o ${FN} ${FN}.sam ; then
+    rm -f ${FN}.sam
+fi
+EOF
+            echo "sbatch .${FN}.sort.sh"
+
+            # Paired
+            FN="r12_${aln}${rdlen}_${genome}_mason_ill_${genome}_${rdlen}"
+            cat >.${FN}.sort.sh <<EOF
+#!/bin/sh
+#SBATCH --job-name=SortPreWasp
+#SBATCH --output ${FN}.out
+#SBATCH --error ${FN}.err
+#SBATCH --cpus-per-task=${NTHREADS}
+${PART1}
+${PART2}
+#SBATCH --time=8:00:00
+#SBATCH --mem=4G
+if sambamba sort -m 3G -t ${NTHREADS} -o ${FN} ${FN}.sam ; then
+    rm -f ${FN}.sam
+fi
+EOF
+            echo "sbatch .${FN}.sort.sh"
+        done
+    done
+done
