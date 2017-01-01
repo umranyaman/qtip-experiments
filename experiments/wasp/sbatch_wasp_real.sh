f27237019409e9298b27fb00b642efbbfbc08c4c
BenLangmead <langmea@cs.jhu.edu>
2017-01-01 11:04:32 -0500

    upping memory limit

--- experiments/wasp/sbatch_wasp_real.sh
+++ experiments/wasp/sbatch_wasp_real.sh
@@ -11,6 +11,8 @@ PART1="#SBATCH --partition=shared"
 PART2=""
 fi
 
+SNPS=human_ensembl85_snps
+
 for dat in ERR050082_1 ERR050083_1 ; do
 for pe in unp pair ; do
 for ext in bwa bt2 snap ; do
@@ -18,21 +20,24 @@ for ext in bwa bt2 snap ; do
 PE_ARG=
 [ "${pe}" = "pair" ] && PE_ARG="--is_paired_end"
 
-cat >.${dat}.${ext}.${pe}.wasp.sh <<EOF
+FN="${dat}.${ext}.${pe}"
+cat >.${FN}.wasp.sh <<EOF
 #!/bin/sh
 
 #SBATCH
 #SBATCH --job-name=Wasp
+#SBATCH --output Wasp.${FN}.out
+#SBATCH --error Wasp.${FN}.err
 #SBATCH --nodes=1
 ${PART1}
 ${PART2}
 #SBATCH --time=8:00:00
 #SBATCH --mem=4G
 
-mkdir -p ${dat}.${ext}.${pe}.sorted.bam.out
-$FIND_SNPS --is_sorted ${PE_ARG} --output_dir ${dat}.${ext}.${pe}.sorted.bam.out --snp_dir snps ../real_data/${dat}.${ext}.${pe}.sorted.bam
+mkdir -p ${FN}.wasp_out
+${FIND_SNPS} --is_sorted ${PE_ARG} --output_dir ${FN}.wasp_out --snp_dir ${SNPS} ${FN}.sorted.bam
 EOF
-echo "sbatch .${dat}.${ext}.${pe}.wasp.sh"
+echo "sbatch .${FN}.wasp.sh"
 
 done
 done
