4242ee749d58078d1579130077bda26198de13f4
BenLangmead <langmea@cs.jhu.edu>
2017-01-11 11:35:40 -0500

    adding more mapq cutoffs

--- experiments/platinum/table_f.py
+++ experiments/platinum/table_f.py
@@ -49,7 +49,7 @@ def go():
                 best_mapq_r = [0] * len(betas)
                 argbest_mapq_r = [None] * len(betas)
 
-                for mapq_cutoff in ['00', '01', '02', '03', '05', '08', '10', '15', '20', '30', 'd', 's', 'u']:
+                for mapq_cutoff in ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '15', '20', '30', 'd', 's', 'u']:
 
                     best_mapq_rc = [0] * len(betas)
                     argbest_mapq_rc = [None] * len(betas)
