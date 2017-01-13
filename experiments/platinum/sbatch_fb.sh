18048f6479be3e085b2176067fc4ea1e6bb6cff0
BenLangmead <langmea@cs.jhu.edu>
2017-01-13 12:02:36 -0500

    upping time and memory limits

--- experiments/platinum/sbatch_fb.sh
+++ experiments/platinum/sbatch_fb.sh
@@ -25,9 +25,9 @@ for CHR in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 ; do
 #SBATCH --output=.CallFB.${NM}_${CHR}_${MINMAPQ}.out
 #SBATCH --error=.CallFB.${NM}_${CHR}_${MINMAPQ}.err
 #SBATCH --nodes=1
-#SBATCH --mem=4G
+#SBATCH --mem=8G
 #SBATCH --partition=shared
-#SBATCH --time=4:00:00
+#SBATCH --time=8:00:00
 FN="${NM}_input_${CHR}_${MINMAPQ}"
 if [ ! -f ERR194147.sam/\${FN}.raw.vcf ] ; then
     ${FB_BASE} -b ERR194147.sam/input.sorted.bam -r ${CHR} ${MINMAPQ_ARG} -v ERR194147.sam/\${FN}.raw.vcf
