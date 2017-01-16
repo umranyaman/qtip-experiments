2050c9427d6507c1bd632cae067dd1767fb1ae82
BenLangmead <langmea@cs.jhu.edu>
2017-01-16 16:00:52 -0500

    change freebayes params and do various coverages

--- experiments/platinum/sbatch_fb.sh
+++ experiments/platinum/sbatch_fb.sh
@@ -2,53 +2,59 @@
 
 NM=ERR194147
 FB=$QTIP_EXPERIMENTS_HOME/software/freebayes/freebayes
-FB_BASE="${FB} -i -X -f $QTIP_EXPERIMENTS_HOME/experiments/refs/hg38.fa"
+FB_BASE="${FB} -X -u --haplotype-length 0 -f $QTIP_EXPERIMENTS_HOME/experiments/refs/hg38.fa"
 VCFLIB_HOME=$QTIP_EXPERIMENTS_HOME/software/vcflib
 VCFISECT=$VCFLIB_HOME/vcflib-git/bin/vcfintersect
-MAXDEPTH_FACTOR=4
-MAXDEPTH=`python -c "from math import *; print(int(round(50 + ${MAXDEPTH_FACTOR} * sqrt(50))))"`
 
-for CHR in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 ; do
-    for MINMAPQ in 00 01 02 03 04 05 06 07 08 09 10 11 12 15 20 30 d s u ; do
-        MINMAPQ_ARG="--min-mapping-quality ${MINMAPQ}"
-        if [ "${MINMAPQ}" = "d" ] ; then
-            MINMAPQ_ARG=""
-        elif [ "${MINMAPQ}" = "s" ] ; then
-            MINMAPQ_ARG="--standard-filters"
-        elif [ "${MINMAPQ}" = "u" ] ; then
-            MINMAPQ_ARG="--use-mapping-quality"
-        fi
-        if [ ! -f "${NM}.sam/${NM}_input_${CHR}_${MINMAPQ}.cr_filt.vcf" -o ! -f "${NM}.sam/${NM}_final_${CHR}_${MINMAPQ}.cr_filt.vcf" ] ; then
-            cat >.CallFB.${NM}_${CHR}_${MINMAPQ}.sh <<EOF
+for COV in 50 40 30 ; do
+
+    MAXDEPTH_FACTOR=4
+    MAXDEPTH=`python -c "from math import *; print(int(round(${COV} + ${MAXDEPTH_FACTOR} * sqrt(${COV}))))"`
+    SUBSAMP_FRAC=`python -c "print(${COV}/52.78920049911709)"`
+
+    for CHR in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 ; do
+        for MINMAPQ in 00 01 02 03 04 05 06 07 08 09 10 11 12 15 20 30 d s u ; do
+            MINMAPQ_ARG="--min-mapping-quality ${MINMAPQ}"
+            if [ "${MINMAPQ}" = "d" ] ; then
+                MINMAPQ_ARG=""
+            elif [ "${MINMAPQ}" = "s" ] ; then
+                MINMAPQ_ARG="--standard-filters"
+            elif [ "${MINMAPQ}" = "u" ] ; then
+                MINMAPQ_ARG="--use-mapping-quality"
+            fi
+            LAB="${NM}_${CHR}_${MINMAPQ}_${COV}"
+            INP_FN="${NM}.sam/${NM}_input_${CHR}_${MINMAPQ}_${COV}"
+            FIN_FN="${NM}.sam/${NM}_final_${CHR}_${MINMAPQ}_${COV}"
+            if [ ! -f "${INP_FN}.cr_filt.vcf" -o ! -f "${FIN_FN}.cr_filt.vcf" ] ; then
+                cat >.CallFB.${LAB}.sh <<EOF
 #!/bin/bash -l
 #SBATCH
 #SBATCH --job-name=CallFB
-#SBATCH --output=.CallFB.${NM}_${CHR}_${MINMAPQ}.out
-#SBATCH --error=.CallFB.${NM}_${CHR}_${MINMAPQ}.err
+#SBATCH --output=.CallFB.${LAB}.out
+#SBATCH --error=.CallFB.${LAB}.err
 #SBATCH --nodes=1
 #SBATCH --mem=8G
 #SBATCH --partition=shared
 #SBATCH --time=8:00:00
-FN="${NM}_input_${CHR}_${MINMAPQ}"
-if [ ! -f ERR194147.sam/\${FN}.raw.vcf ] ; then
-    ${FB_BASE} -b ERR194147.sam/input.sorted.bam -r ${CHR} ${MINMAPQ_ARG} -v ERR194147.sam/\${FN}.raw.vcf
+if [ ! -f ${INP_FN}.raw.vcf ] ; then
+    samtools view -s ${SUBSAMP_FRAC} -bu ERR194147.sam/input.sorted.bam | ${FB_BASE} --stdin -r ${CHR} ${MINMAPQ_ARG} -v ${INP_FN}.raw.vcf
 fi
-if [ ! -f ERR194147.sam/\${FN}.cr_filt.vcf ] ; then
-    ${VCFISECT} -b cr_${CHR}.bed ERR194147.sam/\${FN}.raw.vcf | \
-        gawk '/^#/ {print} match(\$0, /DP=([0-9]+);/, a) {if(a[1] <= ${MAXDEPTH}) {print}}' > ERR194147.sam/\${FN}.cr_filt.vcf
+if [ ! -f ${INP_FN}.cr_filt.vcf ] ; then
+    ${VCFISECT} -b cr_${CHR}.bed ${INP_FN}.raw.vcf | \
+        gawk '/^#/ {print} match(\$0, /DP=([0-9]+);/, a) {if(a[1] <= ${MAXDEPTH}) {print}}' > ${INP_FN}.cr_filt.vcf
 fi
 
-FN="${NM}_final_${CHR}_${MINMAPQ}"
-if [ ! -f ERR194147.sam/\${FN}.raw.vcf ] ; then
-    ${FB_BASE} -b ERR194147.sam/final.sorted.bam -r ${CHR} ${MINMAPQ_ARG} -v ERR194147.sam/\${FN}.raw.vcf
+if [ ! -f ${FIN_FN}.raw.vcf ] ; then
+    samtools view -s ${SUBSAMP_FRAC} -bu ERR194147.sam/final.sorted.bam | ${FB_BASE} --stdin -r ${CHR} ${MINMAPQ_ARG} -v ${FIN_FN}.raw.vcf
 fi
-if [ ! -f ERR194147.sam/\${FN}.cr_filt.vcf ] ; then
-    ${VCFISECT} -b cr_${CHR}.bed ERR194147.sam/\${FN}.raw.vcf | \
-        gawk '/^#/ {print} match(\$0, /DP=([0-9]+);/, a) {if(a[1] <= ${MAXDEPTH}) {print}}' > ERR194147.sam/\${FN}.cr_filt.vcf
+if [ ! -f ${FIN_FN}.cr_filt.vcf ] ; then
+    ${VCFISECT} -b cr_${CHR}.bed ${FIN_FN}.raw.vcf | \
+        gawk '/^#/ {print} match(\$0, /DP=([0-9]+);/, a) {if(a[1] <= ${MAXDEPTH}) {print}}' > ${FIN_FN}.cr_filt.vcf
 fi
 EOF
-            echo "sbatch .CallFB.${NM}_${CHR}_${MINMAPQ}.sh"
-            [ "$1" = "wet" ] && sbatch .CallFB.${NM}_${CHR}_${MINMAPQ}.sh && sleep 1
-        fi
-	done
+                echo "sbatch .CallFB.${LAB}.sh"
+                [ "$1" = "wet" ] && sbatch .CallFB.${LAB}.sh && sleep 1
+            fi
+        done
+    done
 done
