98d6bbf0afab2e51ffcd541120cbc24a507099e8
Ben Langmead <ben.langmead@gmail.com>
2017-01-27 11:46:56 -0500

    switch to whole genome

--- experiments/platinum/table_f.py
+++ experiments/platinum/table_f.py
@@ -39,7 +39,8 @@ def go():
     print()
 
     for filt in ['cr_filt']:
-        for ch in map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 'X']):
+#        for ch in map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 'X']):
+        for ch in map(str, ['W']):
             
             best_mapq = [0] * len(betas)
             argbest_mapq = [None] * len(betas)
