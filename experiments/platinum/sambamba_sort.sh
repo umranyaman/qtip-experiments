ecbb11d36e284474ea8cbca0924d629a8837b157
Ben Langmead <ben.langmead@gmail.com>
2017-04-30 14:25:02 -0400

    sort input bam

--- experiments/platinum/sambamba_sort.sh
+++ experiments/platinum/sambamba_sort.sh
@@ -3,17 +3,20 @@
 #SBATCH --nodes=1
 #SBATCH --mem=110G
 #SBATCH --partition=parallel
-#SBATCH --time=12:00:00
+#SBATCH --time=4:00:00
 
-NM=final
 SAMP=ERR194147
 NTHREADS=24
 MEM="100G"
 
-if [ ! -f "${SAMP}.sam/${NM}.bam" ] ; then
-    sambamba view -S -f bam "${SAMP}.sam/${NM}.sam" > "${SAMP}.sam/${NM}.bam"
-fi
-if [ ! -f "${SAMP}.sam/${NM}.sorted.bam" ] ; then
-    mkdir -p "sambamba_temp_${SAMP}"
-    sambamba sort --tmpdir="sambamba_temp_${SAMP}" -p -m ${MEM} -t ${NTHREADS} "${SAMP}.sam/${NM}.bam" "${SAMP}.sam/${NM}.sorted.bam"
-fi
+for NM in input final ; do
+    if [ ! -f "${SAMP}.sam/${NM}.bam" ] ; then
+	sambamba view -S -f bam "${SAMP}.sam/${NM}.sam" > "${SAMP}.sam/${NM}.bam"
+    fi
+    if [ ! -f "${SAMP}.sam/${NM}.sorted.bam" ] ; then
+	TEMP="sambamba_temp_${SAMP}_${NM}"
+	mkdir -p "${TEMP}"
+	sambamba sort --tmpdir="${TEMP}" -p -m ${MEM} -t ${NTHREADS} "${SAMP}.sam/${NM}.bam" "${SAMP}.sam/${NM}.sorted.bam"
+    fi
+done
+
