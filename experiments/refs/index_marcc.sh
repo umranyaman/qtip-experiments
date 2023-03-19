525ef1db149aa8c9064be5ed6bfc2ba627d0e728
Umran Yaman <72675757+umranyaman@users.noreply.github.com>
2023-03-19 19:22:29 +0000

    Update index_marcc.sh
    Slurm specific commands changed to SGE scheduler

--- experiments/refs/index_marcc.sh
+++ experiments/refs/index_marcc.sh
@@ -5,54 +5,54 @@
 
 do_genome() {
     cat > ".${1}.fa2b.sh" <<EOF
-#!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --mem=8G
-#SBATCH --partition=shared
-#SBATCH --time=2:00:00
-#SBATCH --output=.${1}.fa2b.sh.o
-#SBATCH --error=.${1}.fa2b.sh.e
+#!/bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_vmem=8G
+#$ -l h_rt=2:00:00
+#$ -o .${1}.fa2b.sh.o
+#$ -e .${1}.fa2b.sh.e
 samtools faidx ${1}.fa
 faToTwoBit ${1}.fa ${1}.2bit
 EOF
-    echo sbatch .${1}.fa2b.sh
+    echo qsub .${1}.fa2b.sh
 
     cat > .${1}.bt2.sh <<EOF
-#!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --mem=16G
-#SBATCH --partition=shared
-#SBATCH --time=10:00:00
-#SBATCH --output=.${1}.bt2.sh.o
-#SBATCH --error=.${1}.bt2.sh.e
+#!/bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_vmem=16G
+#$ -l h_rt=10:00:00
+#$ -o .${1}.bt2.sh.o
+#$ -e .${1}.bt2.sh.e
 $QTIP_HOME/software/bowtie2/bowtie2-build --bmax 537647674 --dcv 1024 $QTIP_EXPERIMENTS_HOME/experiments/refs/${1}.fa ${1}.fa
 EOF
-    echo sbatch .${1}.bt2.sh
+    echo qsub .${1}.bt2.sh
 
     cat > .${1}.bwa.sh <<EOF
-#!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --mem=8G
-#SBATCH --partition=shared
-#SBATCH --time=10:00:00
-#SBATCH --output=.${1}.bwa.sh.o
-#SBATCH --error=.${1}.bwa.sh.e
+#!/bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_vmem=8G
+#$ -l h_rt=10:00:00
+#$ -o .${1}.bwa.sh.o
+#$ -e .${1}.bwa.sh.e
 $QTIP_HOME/software/bwa/bwa index $QTIP_EXPERIMENTS_HOME/experiments/refs/${1}.fa
 mv $TS_REFS/${1}.fa.* .
 EOF
-    echo sbatch .${1}.bwa.sh
+    echo qsub .${1}.bwa.sh
 
     cat > .${1}.snap.sh <<EOF
-#!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --mem=60G
-#SBATCH --partition=shared
-#SBATCH --time=2:00:00
-#SBATCH --output=.${1}.snap.sh.o
-#SBATCH --error=.${1}.snap.sh.e
+#!/bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_vmem=60G
+#$ -l h_rt=2:00:00
+#$ -o .${1}.snap.sh.o
+#$ -e .${1}.snap.sh.e
 $QTIP_HOME/software/snap/snap/snap-aligner index $QTIP_EXPERIMENTS_HOME/experiments/refs/${1}.fa ${1}.fa.snap -bSpace
 EOF
-    echo sbatch .${1}.snap.sh
+    echo qsub .${1}.snap.sh
 }
 
 do_genome hg19
